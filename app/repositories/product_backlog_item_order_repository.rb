3497fb56f6e8993c0d626c41b5e66922cf205346
haazime <h4zime@gmail.com>
2020-07-01 20:33:00 +0900

    refactor

--- app/repositories/product_backlog_item_order_repository.rb
+++ app/repositories/product_backlog_item_order_repository.rb
@@ -7,9 +7,11 @@ module ProductBacklogItemOrderRepository
       extend T::Sig
       include Product::BacklogItemOrderRepository
 
+      DAO = Dao::ProductBacklogItemPriority
+
       sig {override.params(product_id: Product::ProductId).returns(T.nilable(Product::BacklogItemOrder))}
       def find_by_product_id(product_id)
-        r = Dao::ProductBacklogItemPriority.where(dao_product_id: product_id.to_s).order(:position)
+        r = DAO.where(dao_product_id: product_id.to_s).order(:position)
         return nil if r.empty?
 
         Product::BacklogItemOrder.from_repository(
@@ -22,11 +24,11 @@ module ProductBacklogItemOrderRepository
       def update(order)
         product_id = order.product_id.to_s
 
-        Dao::ProductBacklogItemPriority.transaction do
-          Dao::ProductBacklogItemPriority.where(dao_product_id: product_id).delete_all
+        DAO.transaction do
+          DAO.where(dao_product_id: product_id).delete_all
 
           order.to_a.each_with_index do |product_backlog_item_id, position|
-            Dao::ProductBacklogItemPriority.create!(
+            DAO.create!(
               dao_product_id: product_id,
               dao_product_backlog_item_id: product_backlog_item_id.to_s,
               position: position,
