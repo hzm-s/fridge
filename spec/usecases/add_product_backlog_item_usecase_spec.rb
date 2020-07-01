47e9d60007b80071d4f430763cb0bcda7009db2f
haazime <h4zime@gmail.com>
2020-07-01 20:27:39 +0900

    signatureがあるため不要

--- spec/usecases/add_product_backlog_item_usecase_spec.rb
+++ spec/usecases/add_product_backlog_item_usecase_spec.rb
@@ -9,22 +9,20 @@ RSpec.describe 'Add product backlog item' do
 
   before { ProductRepository::AR.add(product) }
 
-  context 'パラメータが正しい場合' do
-    it '追加したPBIが保存されていること' do
-      pbi_id = uc.perform(product.id, Product::BacklogItemContent.from_string('aaa'))
-      pbi = pbi_repo.find_by_id(pbi_id)
+  it '追加したPBIが保存されていること' do
+    pbi_id = uc.perform(product.id, Product::BacklogItemContent.from_string('aaa'))
+    pbi = pbi_repo.find_by_id(pbi_id)
 
-      expect(pbi).to_not be_nil
-      expect(pbi.content.to_s).to eq 'aaa'
-    end
+    expect(pbi).to_not be_nil
+    expect(pbi.content.to_s).to eq 'aaa'
+  end
 
-    it '追加したPBIの優先順位は最低になっていること' do
-      uc.perform(product.id, Product::BacklogItemContent.from_string('aaa'))
+  it '追加したPBIの優先順位は最低になっていること' do
+    uc.perform(product.id, Product::BacklogItemContent.from_string('aaa'))
 
-      pbi_id = uc.perform(product.id, Product::BacklogItemContent.from_string('bbb'))
-      order = order_repo.find_by_product_id(product.id)
+    pbi_id = uc.perform(product.id, Product::BacklogItemContent.from_string('bbb'))
+    order = order_repo.find_by_product_id(product.id)
 
-      expect(order.position(pbi_id)).to eq 1
-    end
+    expect(order.position(pbi_id)).to eq 1
   end
 end
