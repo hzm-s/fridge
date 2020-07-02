3213cd3df70d70dfa4b8f28449d83f818b816dd7
haazime <h4zime@gmail.com>
2020-07-02 21:16:48 +0900

    minimal ui

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  resources :product_backlog_items, only: [:create, :index]
+  root to: 'products#index'
+
+  resources :products, only: [:index]
+  resources :product_backlog_items, only: [:index, :new, :create]
 end
