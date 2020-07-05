94c3bbf9f488c4d9147970c1a283d5dbb8ac869d
haazime <h4zime@gmail.com>
2020-07-05 20:08:13 +0900

    add special font

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 module ApplicationHelper
   include BootstrapHelper
   include PbiHelper
+
+  WEB_FONT_URL = 'https://fonts.googleapis.com/earlyaccess/roundedmplus1c.css'.freeze
+
+  def web_font_link_tag
+    return if Rails.env.test?
+    stylesheet_link_tag(WEB_FONT_URL, media: 'all', 'data-turbolinks-track': 'reload')
+  end
 end
