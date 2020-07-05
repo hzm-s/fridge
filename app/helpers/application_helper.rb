8e42f6334a356fa6da4fe1b0ca16499d7abc6e04
haazime <h4zime@gmail.com>
2020-07-05 09:39:50 +0900

    dropdown helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def dd_trigger_options(options)
+    dom_id = options.delete(:id)
+    dom_class = options.delete(:class)
+
+    opts = {
+      data: {
+        toggle: 'dropdown',
+        display: 'static',
+        'aria-haspopup' => 'true',
+        'aria-expanded' => 'false'
+      }
+    }
+
+    opts.merge!(id: dom_id) if dom_id
+    opts.merge!(class: dom_class) if dom_class
+    opts
+  end
 end
