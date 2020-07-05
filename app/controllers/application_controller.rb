b9fd1736526f88db757d5b1f7f64595c03e6b83a
haazime <h4zime@gmail.com>
2020-07-05 14:41:03 +0900

    use live data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user_products
+
+  def current_user_products
+    @__products ||= fetch_current_user_products
+  end
+
+  def fetch_current_user_products
+    Dao::Product.all
+  end
 end
