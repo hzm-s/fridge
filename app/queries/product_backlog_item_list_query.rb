3679e0a2ba229f26d7af31098dc29ade6f03d937
haazime <h4zime@gmail.com>
2020-07-11 16:10:25 +0900

    implement query

--- app/queries/product_backlog_item_list_query.rb
+++ app/queries/product_backlog_item_list_query.rb
@@ -1,7 +1,12 @@
 module ProductBacklogItemListQuery
   class << self
     def call(product_id)
-      Dao::ProductBacklogItem.where(dao_product_id: product_id)
+      order = Dao::ProductBacklogOrder.find_by!(dao_product_id: product_id)
+      items = Dao::ProductBacklogItem.where(dao_product_id: product_id).to_a
+
+      order.product_backlog_item_ids.map do |pbi_id|
+        items.find { |item| item.id == pbi_id }
+      end
     end
   end
 end
---
+++ spec/queries/product_backlog_item_list_query_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe ProductBacklogItemListQuery do
+  it do
+    product = create_product
+
+    pbi_a = add_pbi(product.id, 'AAA')
+    pbi_b = add_pbi(product.id, 'BBB')
+    pbi_c = add_pbi(product.id, 'CCC')
+    ReorderProductBacklogUsecase.new.perform(product.id, pbi_c.id, pbi_b.id)
+
+    item_ids = described_class.call(product.id.to_s).map(&:id)
+    
+    expect(item_ids).to eq [pbi_a, pbi_c, pbi_b].map(&:id).map(&:to_s)
+  end
+end
