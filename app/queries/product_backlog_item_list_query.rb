7a9eac283f0298efc44edf861c4f734167c29f51
haazime <h4zime@gmail.com>
2020-07-12 09:54:29 +0900

    manage empty pbl

--- app/queries/product_backlog_item_list_query.rb
+++ app/queries/product_backlog_item_list_query.rb
@@ -1,12 +1,24 @@
 module ProductBacklogItemListQuery
   class << self
     def call(product_id)
-      order = Dao::ProductBacklogOrder.find_by!(dao_product_id: product_id)
-      items = Dao::ProductBacklogItem.where(dao_product_id: product_id).to_a
+      order = fetch_order(product_id)
+      return [] unless order
+
+      items = fetch_items(product_id)
 
       order.product_backlog_item_ids.map do |pbi_id|
         items.find { |item| item.id == pbi_id }
       end
+    end
+
+    private
+
+    def fetch_order(product_id)
+      Dao::ProductBacklogOrder.find_by(dao_product_id: product_id)
+    end
+
+    def fetch_items(product_id)
+      Dao::ProductBacklogItem.where(dao_product_id: product_id).to_a
     end
   end
 end
--- spec/queries/product_backlog_item_list_query_spec.rb
+++ spec/queries/product_backlog_item_list_query_spec.rb
@@ -1,16 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe ProductBacklogItemListQuery do
-  it do
-    product = create_product
+  let(:product) { create_product }
 
+  it do
     pbi_a = add_pbi(product.id, 'AAA')
     pbi_b = add_pbi(product.id, 'BBB')
     pbi_c = add_pbi(product.id, 'CCC')
     ReorderProductBacklogUsecase.new.perform(product.id, pbi_c.id, 2)
 
     item_ids = described_class.call(product.id.to_s).map(&:id)
-    
+
     expect(item_ids).to eq [pbi_a, pbi_c, pbi_b].map(&:id).map(&:to_s)
+  end
+
+  it do
+    items = described_class.call(product.id.to_s)
+    expect(items).to be_empty
   end
 end
