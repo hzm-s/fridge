3f4af950cbc3673ca3dd4e4df90650f7c1a039dc
haazime <h4zime@gmail.com>
2020-07-12 15:41:23 +0900

    show acceptance criteria

--- spec/requests/acceptance_criteria_spec.rb
+++ spec/requests/acceptance_criteria_spec.rb
@@ -8,10 +8,9 @@ RSpec.describe 'acceptance_criteria' do
     context 'given valid params' do
       it do
         params = { form: { content: 'ukeire_kijyun' } }
-
         post product_backlog_item_acceptance_criteria_path(product_backlog_item_id: pbi.id, format: :js), params: params
-        get product_product_backlog_items_path(product_id: product.id.to_s)
 
+        get product_product_backlog_items_path(product_id: product.id.to_s)
         expect(response.body).to include 'ukeire_kijyun'
       end
     end
