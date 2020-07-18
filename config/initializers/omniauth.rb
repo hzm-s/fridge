f995bda236563f73e0427e95727d68ab037c845a
haazime <h4zime@gmail.com>
2020-07-18 16:23:32 +0900

    fix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, Rails.application.credentials.google[:client_id], Rails.application.credentials.google[:client_secret]
+unless Rails.env.test?
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :google_oauth2, Rails.application.credentials.google[:client_id], Rails.application.credentials.google[:client_secret]
+  end
 end
