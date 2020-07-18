95fc85e759d0d375db89018c902b7ad018ec7e0c
haazime <h4zime@gmail.com>
2020-07-18 21:49:07 +0900

    show avatar with initials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,21 @@
 # typed: true
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  protected
+
   def flash_success(key)
     { success: I18n.t(key, scope: 'feedbacks') }
+  end
+
+  def current_user
+    @__current_user ||= fetch_current_user
+  end
+
+  private
+
+  def fetch_current_user
+    return nil unless user_id = session[:user_id]
+    UserRepository::AR.find_by_id(user_id)
   end
 end
