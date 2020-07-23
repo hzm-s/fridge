fa33cb36b5569318152646adb012a52647eec41c
haazime <h4zime@gmail.com>
2020-07-23 15:27:28 +0900

    show member role

---
+++ app/controllers/concerns/product_helper.rb
@@ -0,0 +1,20 @@
+# typed: false
+module ProductHelper
+  extend ActiveSupport::Concern
+
+  included do
+    helper_method :current_product, :current_team_member
+  end
+
+  def current_product_id
+    raise 'not implemented'
+  end
+
+  def current_product
+    Dao::Product.find_by(id: current_product_id)
+  end
+
+  def current_team_member
+    Dao::TeamMember.find_by(dao_user_id: current_user.id, dao_product_id: current_product_id)
+  end
+end
