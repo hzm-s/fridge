77d936bf4fb11f7006360b8f59f332056c86fb94
haazime <h4zime@gmail.com>
2020-07-25 10:31:06 +0900

    change sut

---
+++ spec/domain/team/team_spec.rb
@@ -0,0 +1,37 @@
+# typed: false
+require 'rails_helper'
+
+module Team
+  RSpec.describe Team do
+    describe '#add_member' do
+      let(:po) { User::User.create('po', 'po@e.mail') }
+      let(:sm) { User::User.create('sm', 'sm@e.mail') }
+      let(:new_member) { User::User.create('new member', 'new.member@e.mail') } 
+
+      before do
+        @team = described_class.new([])
+        @team = @team.add_member(po_member(po.id))
+        @team = @team.add_member(sm_member(sm.id))
+        9.times do |n|
+          dev = User::User.create("dev#{n}", "dev#{n}@e.mail")
+          @team = @team.add_member(dev_member(dev.id))
+        end
+      end
+
+      it '2人目のプロダクトオーナーはエラーになること' do
+        expect { @team.add_member(po_member(new_member.id)) }
+          .to raise_error(DuplicateProductOwnerError)
+      end
+
+      it '2人目のスクラムマスターはエラーになること' do
+        expect { @team.add_member(sm_member(new_member.id)) }
+          .to raise_error(DuplicateScrumMasterError)
+      end
+
+      it '10人目の開発者はエラーになること' do
+        expect { @team.add_member(dev_member(new_member.id)) }
+          .to raise_error(LargeDevelopmentTeamError)
+      end
+    end
+  end
+end
