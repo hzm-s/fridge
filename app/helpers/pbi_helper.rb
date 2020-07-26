ce2ccb8c2ce1c7c12076bb5d539ee3ce2ab19a3f
haazime <h4zime@gmail.com>
2020-07-26 20:32:09 +0900

    add ready and wip filter

--- app/helpers/pbi_helper.rb
+++ app/helpers/pbi_helper.rb
@@ -1,10 +1,20 @@
 # typed: true
 module PbiHelper
+  STATUS_FILTER_CLASSES_BASE = 'pbl__filter btn'.freeze
+
   def present_story_point(point)
     Pbi::StoryPoint.new(point).to_s
   end
 
   def all_story_points
     Pbi::StoryPoint.all.map(&:to_s)
+  end
+
+  def all_pbi_statuses
+    Pbi::Statuses.all.map(&:to_s)
+  end
+
+  def pbl_status_filter_classes(current:, filter:)
+    "#{STATUS_FILTER_CLASSES_BASE} btn-#{current == filter ? '' : 'outline-'}secondary"
   end
 end
---
+++ domain/pbi/statuses/wip.rb
@@ -0,0 +1,23 @@
+# typed: strict
+require 'sorbet-runtime'
+
+module Pbi
+  module Statuses
+    module Wip
+      class << self
+        extend T::Sig
+        include Status
+
+        sig {override.params(pbi: Item).returns(Status)}
+        def update_by(pbi)
+          self
+        end
+
+        sig {override.returns(String)}
+        def to_s
+          'wip'
+        end
+      end
+    end
+  end
+end
