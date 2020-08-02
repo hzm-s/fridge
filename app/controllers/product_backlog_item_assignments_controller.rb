5935a79c29c906579e65e3c681244c9b777f7f0d
haazime <h4zime@gmail.com>
2020-08-02 16:53:18 +0900

    implement ui

--- app/controllers/product_backlog_item_assignments_controller.rb
+++ app/controllers/product_backlog_item_assignments_controller.rb
@@ -3,5 +3,13 @@ class ProductBacklogItemAssignmentsController < ApplicationController
     pbi_id = Pbi::Id.from_string(params[:product_backlog_item_id])
     AssignProductBacklogItemUsecase.perform(pbi_id)
     @item = ProductBacklogItemQuery.call(pbi_id.to_s)
+    render :show
+  end
+
+  def destroy
+    pbi_id = Pbi::Id.from_string(params[:id])
+    CancelProductBacklogItemAssignmentUsecase.perform(pbi_id)
+    @item = ProductBacklogItemQuery.call(pbi_id.to_s)
+    render :show
   end
 end
--- spec/requests/product_backlog_item_assignment_spec.rb
+++ spec/requests/product_backlog_item_assignment_spec.rb
@@ -5,17 +5,39 @@ RSpec.describe 'product_backlog_item_assignment' do
   let(:product) { create_product }
   let!(:pbi) { add_pbi(product.id, acceptance_criteria: %w(ac1), size: 2) }
 
-  it do
-    expect {
+  describe '#create' do
+    it do
+      expect {
+        post product_backlog_item_assignments_path(product_backlog_item_id: pbi.id.to_s, format: :js)
+      }
+        .to change { Dao::ProductBacklogItem.find(pbi.id.to_s).status }
+        .from(Pbi::Statuses::Ready.to_s)
+        .to(Pbi::Statuses::Todo.to_s)
+    end
+
+    it do
       post product_backlog_item_assignments_path(product_backlog_item_id: pbi.id.to_s, format: :js)
-    }
-      .to change { Dao::ProductBacklogItem.find(pbi.id.to_s).status }
-      .from(Pbi::Statuses::Ready.to_s)
-      .to(Pbi::Statuses::Todo.to_s)
+      expect(response.body).to include 'todo'
+    end
   end
 
-  it do
-    post product_backlog_item_assignments_path(product_backlog_item_id: pbi.id.to_s, format: :js)
-    expect(response.body).to include 'todo'
+  describe '#destroy' do
+    before do
+      assign_pbi(pbi.id)
+    end
+
+    it do
+      expect {
+        delete product_backlog_item_assignment_path(pbi.id.to_s, format: :js)
+      }
+        .to change { Dao::ProductBacklogItem.find(pbi.id.to_s).status }
+        .from(Pbi::Statuses::Todo.to_s)
+        .to(Pbi::Statuses::Ready.to_s)
+    end
+
+    it do
+      delete product_backlog_item_assignment_path(pbi.id.to_s, format: :js)
+      expect(response.body).to include 'ready'
+    end
   end
 end
