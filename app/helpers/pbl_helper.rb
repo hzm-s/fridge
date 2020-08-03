97adfb8a5e17829ac81bd928750c1d467f21d701
haazime <h4zime@gmail.com>
2020-08-03 19:41:17 +0900

    wip

--- app/helpers/pbl_helper.rb
+++ app/helpers/pbl_helper.rb
@@ -5,4 +5,12 @@ module PblHelper
   def pbl_status_filter_classes(current:, filter:)
     "#{STATUS_FILTER_CLASSES_BASE} btn-#{current == filter ? '' : 'outline-'}secondary"
   end
+
+  def sortable_pbl_options(product_id)
+    {
+      controller: 'sort-pbl',
+      sort_pbl_handle: '.pbl-item__grip',
+      sort_pbl_url: product_product_backlog_item_order_path(product_id: product_id)
+    }
+  end
 end
