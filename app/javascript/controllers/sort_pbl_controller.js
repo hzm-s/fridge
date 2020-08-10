622a1716eb1574728095aac88a93c7daaa1a7934
haazime <h4zime@gmail.com>
2020-08-10 10:33:49 +0900

    refactor sortable wrapper

--- app/javascript/controllers/sort_pbl_controller.js
+++ app/javascript/controllers/sort_pbl_controller.js
@@ -1,6 +1,14 @@
 import { Controller } from 'stimulus';
 import Sortable, { AutoScroll } from 'sortablejs/modular/sortable.core.esm.js'
 
+const uiOptions = {
+  handle: '.js-sortable-handle',
+  scroll: true,
+  animation: 150,
+  easing: 'cubic-bezier(1, 0, 0, 1)',
+  ghostClass: '.sortable-ghost'
+}
+
 export default class extends Controller {
   initialize() {
     Sortable.mount(new AutoScroll())
@@ -8,16 +16,19 @@ export default class extends Controller {
 
   connect() {
     const url = this.data.get('url')
+    const groupName = this.element.id
 
     Sortable.create(this.element, {
-      handle: this.data.get('handle'),
-      scroll: true,
-      animation: 150,
-      easing: 'cubic-bezier(1, 0, 0, 1)',
-      ghostClass: '.sortable-ghost',
+      ...uiOptions,
+      group: {
+        name: groupName,
+        pull: true,
+        put: true
+      },
       onEnd: function(e) {
         const payload = {
-          pbi_id: e.item.dataset.sortableId,
+          pbi_id: e.item.dataset.pbiId,
+          release: e.to.dataset.release,
           to: e.newIndex + 1
         }
         $.ajax({ type: 'PATCH', url: url, dataType: 'json', data: payload })
