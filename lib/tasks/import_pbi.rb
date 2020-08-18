166fe2930d3f875105781d4ec6e99dd34ea25c0f
haazime <h4zime@gmail.com>
2020-08-18 19:48:49 +0900

    import & export pbis

---
+++ lib/tasks/import_pbi.rb
@@ -0,0 +1,13 @@
+data = JSON.parse(File.read(ARGV[0])).deep_symbolize_keys
+
+data[:pbis].each do |i|
+  p Dao::ProductBacklogItem.new(i).save!
+end
+
+data[:criteria].each do |c|
+  p Dao::AcceptanceCriterion.new(c).save!
+end
+
+data[:releases].each do |r|
+  Dao::Release.create!(r)
+end
