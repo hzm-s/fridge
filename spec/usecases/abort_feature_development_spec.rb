5a8135b2725711a214ea33f84485f2f15b58214b
haazime <h4zime@gmail.com>
2020-08-23 18:06:46 +0900

    usecase for abort development

--- spec/usecases/abort_feature_development_spec.rb
+++ spec/usecases/abort_feature_development_spec.rb
@@ -1,15 +1,16 @@
 # typed: false
 require 'rails_helper'
 
-RSpec.describe CancelProductBacklogItemAssignmentUsecase do
+RSpec.describe AbortFeatureDevelopmentUsecase do
   let(:product) { create_product }
   let!(:feature) { add_feature(product.id, acceptance_criteria: %w(ac1), size: 8) }
 
   it do
     start_feature_development(feature.id)
 
-    described_class.perform(pbi.id)
-    updated = ProductBacklogItemRepository::AR.find_by_id(pbi.id)
-    expect(updated.status).to eq Pbi::Statuses::Ready
+    described_class.perform(feature.id)
+    updated = FeatureRepository::AR.find_by_id(feature.id)
+
+    expect(updated.status).to eq Feature::Statuses::Ready
   end
 end
