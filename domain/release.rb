84e0dc064e77bfbc01d8b97c27d812a9c049ccbe
haazime <h4zime@gmail.com>
2020-08-23 10:04:55 +0900

    mark as omit

--- domain/release.rb
+++ domain/release.rb
@@ -4,9 +4,8 @@ module Release
   class CanNotRemoveRelease < StandardError; end
 
   autoload :ReleaseRepository, 'release/release_repository'
-
   autoload :Id, 'release/id'
   autoload :Release, 'release/release'
-
+  # TODO: omit
   autoload :ItemSorter, 'release/item_sorter'
 end
