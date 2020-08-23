4631548726e47ebab9c03601f2576aac31f53c0c
haazime <h4zime@gmail.com>
2020-08-23 10:07:25 +0900

    clean up

--- domain/release.rb
+++ domain/release.rb
@@ -1,11 +1,7 @@
 # typed: false
 
 module Release
-  class CanNotRemoveRelease < StandardError; end
-
   autoload :ReleaseRepository, 'release/release_repository'
   autoload :Id, 'release/id'
   autoload :Release, 'release/release'
-  # TODO: omit
-  autoload :ItemSorter, 'release/item_sorter'
 end
