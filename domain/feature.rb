4b1f28c4cc638f74bbf1958eb904d00630a9a0f2
haazime <h4zime@gmail.com>
2020-08-23 12:06:17 +0900

    Description

--- domain/feature.rb
+++ domain/feature.rb
@@ -1,11 +1,13 @@
 # typed: strict
 module Feature
+  class InvalidDescription < ArgumentError; end
   class AssignProductBacklogItemNotAllowed < StandardError; end
   class ProductBacklogItemIsNotAssigned < StandardError; end
   class ItemCanNotRemove < StandardError; end
 
   autoload :Id, 'feature/id'
   autoload :Feature, 'feature/feature'
+  autoload :Description, 'feature/description'
   autoload :AcceptanceCriterion, 'feature/acceptance_criterion'
   autoload :AcceptanceCriteria, 'feature/acceptance_criteria'
   autoload :StoryPoint, 'feature/story_point'
--- domain/pbi.rb
+++ domain/pbi.rb
@@ -1,7 +1,5 @@
 # typed: strict
 module Pbi
-  class InvalidContent < ArgumentError; end
-
   autoload :Id, 'pbi/id'
   autoload :Item, 'pbi/item'
   autoload :Content, 'pbi/content'
