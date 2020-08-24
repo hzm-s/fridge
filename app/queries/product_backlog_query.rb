acffa9ea5b2886f19f6e0978b3734fa6d5e085e4
haazime <h4zime@gmail.com>
2020-08-24 21:03:49 +0900

    improve query

--- app/queries/product_backlog_query.rb
+++ app/queries/product_backlog_query.rb
@@ -2,36 +2,19 @@
 require 'sorbet-runtime'
 
 module ProductBacklogQuery
-  class Item < SimpleDelegator
-    def status
-      @__status ||= Feature::Statuses.from_string(super)
-    end
-  end
-
-  class Release
-    def initialize(pbl, all)
-      @pbl = pbl
-      @all = all
-    end
-
-    def items
-      @__items ||= @pbl.items.map { |item| @all.find { |f| f.id == item.to_s } }
-    end
-  end
-
   class << self
     def call(product_id)
-      pbl = fetch_product_backlog(product_id)
-      return [] unless pbl
-
-      features = fetch_features(product_id)
-      [Release.new(pbl, features)]
+      ProductBacklog.new(
+        ordered_item_ids: fetch_ordered_item_ids(product_id),
+        releases: fetch_releases(product_id),
+        features: fetch_features(product_id)
+      )
     end
 
     private
 
-    def fetch_product_backlog(product_id)
-      ProductBacklogRepository::AR.find_by_product_id(Product::Id.from_string(product_id))
+    def fetch_ordered_item_ids(product_id)
+      Dao::ProductBacklog.find_by(dao_product_id: product_id).items
     end
 
     def fetch_features(product_id)
@@ -39,6 +22,42 @@ module ProductBacklogQuery
         .eager_load(:criteria)
         .where(dao_product_id: product_id)
         .map { |r| Item.new(r) }
+    end
+
+    def fetch_releases(product_id)
+      ReleaseRepository::AR.all_by_product_id(Product::Id.from_string(product_id))
+    end
+  end
+
+  class ProductBacklog
+
+    def initialize(ordered_item_ids:, releases:, features:)
+      @ordered_item_ids = ordered_item_ids
+      @releases = releases
+      @features = features
+    end
+
+    def items
+      @__items ||= @ordered_item_ids.map { |o| @features.find { |f| f.id == o } }
+    end
+
+    def releases
+      @__releases ||= @releases.map { |r| Release.new(r, items.select { |i| r.items.include?(i.id) }) }
+    end
+  end
+
+  class Release < SimpleDelegator
+    attr_reader :items
+    
+    def initialize(release, items)
+      super(release)
+      @items = items
+    end
+  end
+
+  class Item < SimpleDelegator
+    def status
+      @__status ||= Feature::Statuses.from_string(super)
     end
   end
 end
