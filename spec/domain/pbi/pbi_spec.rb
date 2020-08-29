a22a6c988f2e4f822a29b7e588e9ffbd74197c56
haazime <h4zime@gmail.com>
2020-08-29 10:25:11 +0900

    feature => pbi

--- spec/domain/pbi/pbi_spec.rb
+++ spec/domain/pbi/pbi_spec.rb
@@ -4,75 +4,75 @@ require 'domain_helper'
 module Pbi
   RSpec.describe Pbi do
     let(:product_id) { Product::Id.create }
-    let(:description) { feature_description('A user story') }
+    let(:description) { pbi_description('A user story') }
 
     describe 'create' do
-      let(:feature) { described_class.create(product_id, description) }
+      let(:pbi) { described_class.create(product_id, description) }
 
       it do
         aggregate_failures do
-          expect(feature.product_id).to eq product_id
-          expect(feature.id).to_not be_nil
-          expect(feature.description).to eq description
+          expect(pbi.product_id).to eq product_id
+          expect(pbi.id).to_not be_nil
+          expect(pbi.description).to eq description
         end
       end
 
       it do
-        expect(feature.size).to eq StoryPoint.unknown
+        expect(pbi.size).to eq StoryPoint.unknown
       end
 
       it do
-        expect(feature.acceptance_criteria).to be_empty
+        expect(pbi.acceptance_criteria).to be_empty
       end
     end
 
     describe 'Modify description' do
-      let(:feature) { described_class.create(product_id, feature_description('Origin')) }
+      let(:pbi) { described_class.create(product_id, pbi_description('Origin')) }
 
       it do
-        new_desc = feature_description('Modified')
-        feature.modify_description(new_desc)
-        expect(feature.description).to eq new_desc
+        new_desc = pbi_description('Modified')
+        pbi.modify_description(new_desc)
+        expect(pbi.description).to eq new_desc
       end
     end
 
     describe 'Acceptance criteria' do
-      let(:feature) { described_class.create(product_id, description) }
+      let(:pbi) { described_class.create(product_id, description) }
 
       it do
         criteria = acceptance_criteria(%w(AC1 AC2 AC3))
-        feature.update_acceptance_criteria(criteria)
-        expect(feature.acceptance_criteria).to eq criteria
+        pbi.update_acceptance_criteria(criteria)
+        expect(pbi.acceptance_criteria).to eq criteria
       end
     end
 
     describe 'Update Status' do
-      let(:feature) { described_class.create(product_id, description) }
+      let(:pbi) { described_class.create(product_id, description) }
 
       it do
-        expect(feature.status).to eq Statuses::Preparation
+        expect(pbi.status).to eq Statuses::Preparation
 
-        feature.modify_description(feature_description('NEW user story'))
-        expect(feature.status).to eq Statuses::Preparation
+        pbi.modify_description(pbi_description('NEW user story'))
+        expect(pbi.status).to eq Statuses::Preparation
 
-        feature.update_acceptance_criteria(acceptance_criteria(%w(AC1)))
-        expect(feature.status).to eq Statuses::Preparation
+        pbi.update_acceptance_criteria(acceptance_criteria(%w(AC1)))
+        expect(pbi.status).to eq Statuses::Preparation
 
-        feature.estimate(StoryPoint.new(3))
-        expect(feature.status).to eq Statuses::Ready
+        pbi.estimate(StoryPoint.new(3))
+        expect(pbi.status).to eq Statuses::Ready
 
-        feature.update_acceptance_criteria(acceptance_criteria([]))
-        expect(feature.status).to eq Statuses::Preparation
+        pbi.update_acceptance_criteria(acceptance_criteria([]))
+        expect(pbi.status).to eq Statuses::Preparation
 
-        feature.update_acceptance_criteria(acceptance_criteria(%w(AC1 AC2 AC3)))
-        feature.start_development
-        expect(feature.status).to eq Statuses::Wip
+        pbi.update_acceptance_criteria(acceptance_criteria(%w(AC1 AC2 AC3)))
+        pbi.start_development
+        expect(pbi.status).to eq Statuses::Wip
 
-        feature.estimate(StoryPoint.new(5))
-        expect(feature.size).to eq StoryPoint.new(3)
+        pbi.estimate(StoryPoint.new(5))
+        expect(pbi.size).to eq StoryPoint.new(3)
 
-        feature.abort_development
-        expect(feature.status).to eq Statuses::Ready
+        pbi.abort_development
+        expect(pbi.status).to eq Statuses::Ready
       end
     end
   end
