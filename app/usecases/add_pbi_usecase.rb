09dbf40e1187cbe4ad251f9b787961ae5d7b420a
haazime <h4zime@gmail.com>
2020-08-30 13:34:58 +0900

    add pbi to release

--- app/usecases/add_pbi_usecase.rb
+++ app/usecases/add_pbi_usecase.rb
@@ -10,14 +10,10 @@ class AddPbiUsecase < UsecaseBase
     @plan_repository = T.let(PlanRepository::AR, Plan::PlanRepository)
   end
 
-  sig {params(product_id: Product::Id, description: Pbi::Description).returns(Pbi::Id)}
-  def perform(product_id, description)
+  sig {params(product_id: Product::Id, description: Pbi::Description, release_no: Integer).returns(Pbi::Id)}
+  def perform(product_id, description, release_no = 1)
     pbi = Pbi::Pbi.create(product_id, description)
-
-    plan = @plan_repository.find_by_product_id(product_id)
-    release = plan.release(1)
-    release.add_item(pbi.id)
-    plan.replace_release(1, release)
+    plan = update_plan(product_id, pbi.id, release_no)
 
     transaction do
       @pbi_repository.add(pbi)
@@ -25,5 +21,18 @@ class AddPbiUsecase < UsecaseBase
     end
 
     pbi.id
+  end
+
+  private
+
+  sig {params(product_id: Product::Id, pbi_id: Pbi::Id, release_no: Integer).returns(Plan::Plan)}
+  def update_plan(product_id, pbi_id, release_no)
+    plan = @plan_repository.find_by_product_id(product_id)
+
+    release = plan.release(release_no)
+    release.add_item(pbi_id)
+
+    plan.replace_release(release_no, release)
+    plan
   end
 end
--- spec/usecases/sort_product_backlog_usecase_spec.rb
+++ spec/usecases/sort_product_backlog_usecase_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe SortProductBacklogUsecase do
   let!(:pbi_b) { add_pbi(product.id).id }
   let!(:pbi_c) { add_pbi(product.id).id }
 
-  context 'same release' do
+  context '同じリリース' do
     it do
       described_class.perform(product.id, 1, pbi_a, 1, 3)
 
@@ -17,18 +17,30 @@ RSpec.describe SortProductBacklogUsecase do
     end
   end
 
-  context 'another release' do
-    it do
+  context '別のリリース' do
+    before do
       add_release(product.id, 'R2')
+    end
 
-      described_class.perform(product.id, 1, pbi_b, 2, 1)
+    context 'リリースに最初のアイテムを追加' do
+      it do
+        described_class.perform(product.id, 1, pbi_b, 2, 1)
 
-      plan = PlanRepository::AR.find_by_product_id(product.id)
-      src = plan.release(1)
-      dst = plan.release(2)
+        plan = PlanRepository::AR.find_by_product_id(product.id)
+        src = plan.release(1)
+        dst = plan.release(2)
 
-      expect(src.items).to eq [pbi_a, pbi_c]
-      expect(dst.items).to eq [pbi_b]
+        expect(src.items).to eq [pbi_a, pbi_c]
+        expect(dst.items).to eq [pbi_b]
+      end
+    end
+
+    context '既存のアイテムと入れ替え' do
+      before do
+      end
+
+      it do
+      end
     end
   end
 end
