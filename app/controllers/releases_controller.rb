42779e5ad6e1767ccaa821aacccd99d12cf1907a
haazime <h4zime@gmail.com>
2020-08-30 09:36:12 +0900

    Plan::Release

--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -2,6 +2,8 @@
 class ReleasesController < ApplicationController
   include ProductHelper
 
+  helper_method :current_product_id
+
   def new
     @form = ReleaseForm.new
   end
@@ -19,8 +21,7 @@ class ReleasesController < ApplicationController
   end
 
   def edit
-    release_id = Release::Id.from_string(params[:id])
-    release = ReleaseRepository::AR.find_by_id(release_id)
+    release = fetch_release
     @form = ReleaseForm.new(title: release.title)
   end
 
@@ -28,22 +29,22 @@ class ReleasesController < ApplicationController
     @form = ReleaseForm.new(release_params)
 
     if @form.valid?
-      release = current_release
-      ModifyReleaseTitleUsecase.perform(release.id, @form.title)
-      redirect_to product_pbis_path(product_id: release.product_id.to_s), flash: flash_success('release.update')
+      product_id = Product::Id.from_string(current_product_id)
+      ModifyReleaseTitleUsecase.perform(product_id, params[:no].to_i, @form.title)
+      redirect_to product_pbis_path(product_id: product_id.to_s), flash: flash_success('release.update')
     else
       render :edit
     end
   end
 
   def destroy
-    release = current_release
-    RemoveReleaseUsecase.perform(release.id)
+    product_id = Product::Id.from_string(current_product_id)
+    RemoveReleaseUsecase.perform(product_id, params[:no].to_i)
     flash = flash_success('release.destroy')
   rescue Release::CanNotRemoveRelease => e
     flash = { notice: t_domain_error(e) }
   ensure
-    redirect_to product_pbis_path(product_id: release.product_id.to_s), flash: flash
+    redirect_to product_pbis_path(product_id: product_id.to_s), flash: flash
   end
 
   private
@@ -52,14 +53,14 @@ class ReleasesController < ApplicationController
     params.require(:form).permit(:title)
   end
 
-  def current_release
-    @__current_release = ReleaseRepository::AR.find_by_id(Release::Id.from_string(params[:id]))
+  def fetch_release
+    product_id = Product::Id.from_string(params[:product_id])
+    no = Release::Id.from_string(params[:no]).to_i
+
+    PlanRepository::AR.find_by_product_id(product_id).release(no)
   end
 
   def current_product_id
-    product_id = params[:product_id]
-    return product_id if product_id
-
-    current_release.product_id.to_s
+    params[:product_id]
   end
 end
