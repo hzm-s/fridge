9eb4862c3ae1b6ee8eebc40edca335bff156f57c
haazime <h4zime@gmail.com>
2020-08-30 13:44:19 +0900

    sort pbl over release

--- spec/usecases/sort_product_backlog_usecase_spec.rb
+++ spec/usecases/sort_product_backlog_usecase_spec.rb
@@ -37,9 +37,21 @@ RSpec.describe SortProductBacklogUsecase do
 
     context '既存のアイテムと入れ替え' do
       before do
+        add_release(product.id, 'R2')
       end
 
       it do
+        pbi_x = add_pbi(product.id, release: 2).id
+        pbi_y = add_pbi(product.id, release: 2).id
+
+        described_class.perform(product.id, 1, pbi_b, 2, 1)
+
+        plan = PlanRepository::AR.find_by_product_id(product.id)
+        src = plan.release(1)
+        dst = plan.release(2)
+
+        expect(src.items).to eq [pbi_a, pbi_c]
+        expect(dst.items).to eq [pbi_b, pbi_x, pbi_y]
       end
     end
   end
