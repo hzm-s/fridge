3461f212880d6ea5bf44c4addcd342f35d208c7a
haazime <h4zime@gmail.com>
2020-09-04 19:28:49 +0900

    fix

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -16,8 +16,8 @@ RSpec.describe 'products' do
         get products_path
 
         aggregate_failures do
-          expect(response.body).to include('fridge')
-          expect(response.body).to include('setsumei_of_product')
+          expect(response.body).to include 'fridge'
+          expect(response.body).to include 'setsumei_of_product'
         end
       end
     end
@@ -27,7 +27,7 @@ RSpec.describe 'products' do
         params = { form: { name: '' } }
         post products_path(format: :js), params: params
 
-        expect(response.body).to include(I18n.t('errors.messages.blank'))
+        expect(response.body).to include I18n.t('errors.messages.blank')
       end
     end
   end
