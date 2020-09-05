0bba52e93f21205713ea40e99a63fb24758cfde6
haazime <h4zime@gmail.com>
2020-09-05 18:56:52 +0900

    fix

--- app/models/dao/team_member.rb
+++ app/models/dao/team_member.rb
@@ -1,5 +1,5 @@
 # typed: strict
 class Dao::TeamMember < ApplicationRecord
-  belongs_to :team, class_name: 'Dao::Team', foreign_key: :dao_team_id
+  belongs_to :team, class_name: 'Dao::Team', foreign_key: :dao_team_id, optional: true
   belongs_to :person, class_name: 'Dao::Person', foreign_key: :dao_person_id
 end
