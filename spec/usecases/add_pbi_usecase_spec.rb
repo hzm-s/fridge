b881949660a7dbac8927af70ce0f230f2ff0787e
haazime <h4zime@gmail.com>
2020-09-12 16:40:38 +0900

    fix

--- spec/usecases/add_pbi_usecase_spec.rb
+++ spec/usecases/add_pbi_usecase_spec.rb
@@ -3,13 +3,12 @@ require 'rails_helper'
 
 RSpec.describe AddPbiUsecase do
   let!(:product) { create_product }
-  let(:pbi_repo) { PbiRepository::AR }
-  let(:plan_repo) { PlanRepository::AR }
 
-  it '追加したアイテムが保存されていること' do
+  it do
     description = pbi_description('ABC')
+
     pbi_id = described_class.perform(product.id, description)
-    pbi = pbi_repo.find_by_id(pbi_id)
+    pbi = PbiRepository::AR.find_by_id(pbi_id)
 
     aggregate_failures do
       expect(pbi.product_id).to eq product.id
@@ -18,26 +17,5 @@ RSpec.describe AddPbiUsecase do
       expect(pbi.size).to eq Pbi::StoryPoint.unknown
       expect(pbi.acceptance_criteria).to be_empty
     end
-  end
-
-  it '追加したフィーチャーの優先順位は最低になっていること' do
-    base_item = described_class.perform(product.id, pbi_description('aaa'))
-
-    new_item = described_class.perform(product.id, pbi_description('bbb'))
-    plan = plan_repo.find_by_product_id(product.id)
-
-    expect(plan.release(1).items).to eq [base_item, new_item]
-  end
-
-  it 'リリースを指定して追加できること' do
-    add_release(product.id, 'R2')
-
-    description = pbi_description('R2_ITEM')
-    pbi_id = described_class.perform(product.id, description, 2)
-
-    plan = PlanRepository::AR.find_by_product_id(product.id)
-
-    expect(plan.release(1).items).to be_empty
-    expect(plan.release(2).items).to eq [pbi_id]
   end
 end
