35b58f657aa3fb363cd20463c109b3327fd9d7e9
haazime <h4zime@gmail.com>
2020-09-16 19:38:31 +0900

    fix

--- spec/usecases/add_acceptance_criterion_usecase_spec.rb
+++ spec/usecases/add_acceptance_criterion_usecase_spec.rb
@@ -3,16 +3,16 @@ require 'rails_helper'
 
 RSpec.describe AddAcceptanceCriterionUsecase do
   let!(:product) { create_product }
-  let!(:pbi) { add_pbi(product.id) }
+  let!(:issue) { add_issue(product.id) }
 
   it do
-    described_class.perform(pbi.id, acceptance_criterion('AC1'))
-    updated = PbiRepository::AR.find_by_id(pbi.id)
-    expect(updated.acceptance_criteria).to eq acceptance_criteria(%w(AC1))
+    described_class.perform(issue.id, acceptance_criterion('AC1'))
+    stored = IssueRepository::AR.find_by_id(issue.id)
+    expect(stored.acceptance_criteria).to eq acceptance_criteria(%w(AC1))
 
-    described_class.perform(pbi.id, acceptance_criterion('AC2'))
-    described_class.perform(pbi.id, acceptance_criterion('AC3'))
-    updated = PbiRepository::AR.find_by_id(pbi.id)
-    expect(updated.acceptance_criteria).to eq acceptance_criteria(%w(AC1 AC2 AC3))
+    described_class.perform(issue.id, acceptance_criterion('AC2'))
+    described_class.perform(issue.id, acceptance_criterion('AC3'))
+    stored = IssueRepository::AR.find_by_id(issue.id)
+    expect(stored.acceptance_criteria).to eq acceptance_criteria(%w(AC1 AC2 AC3))
   end
 end
