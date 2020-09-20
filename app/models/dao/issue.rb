ba0b5b452c4cdb709f66f27a32851aa965c9b098
haazime <h4zime@gmail.com>
2020-09-20 20:12:25 +0900

    for strict

--- app/models/dao/issue.rb
+++ app/models/dao/issue.rb
@@ -4,4 +4,55 @@ class Dao::Issue < ApplicationRecord
   has_one :release_item, class_name: 'Dao::ReleaseItem', foreign_key: :dao_issue_id
   
   delegate :dao_release_id, to: :release_item, allow_nil: true
+
+  def write(issue)
+    self.attributes = {
+      dao_product_id: issue.product_id.to_s,
+      status: issue.status.to_s,
+      description: issue.description.to_s,
+      size: issue.size.to_i,
+    }
+
+    self.criteria.clear
+    issue.acceptance_criteria.to_a.each do |ac|
+      self.criteria.build(content: ac.to_s)
+    end
+  end
+
+  def read
+    Issue::Issue.from_repository(
+      read_issue_id,
+      read_product_id,
+      read_status,
+      read_description,
+      read_story_point,
+      read_acceptance_criteria
+    )
+  end
+
+  def read_issue_id
+    Issue::Id.from_string(id)
+  end
+
+  def read_product_id
+    Product::Id.from_string(dao_product_id)
+  end
+
+  def read_status
+    Issue::Statuses.from_string(status)
+  end
+
+  def read_description
+    Issue::Description.new(description)
+  end
+
+  def read_story_point
+    Issue::StoryPoint.new(size)
+  end
+
+  def read_acceptance_criteria
+    criteria
+      .map { |c| Issue::AcceptanceCriterion.new(c.content) }
+      .yield_self { |c| Issue::AcceptanceCriteria.new(c) }
+  end
 end
--- app/models/dao/release.rb
+++ app/models/dao/release.rb
@@ -1,4 +1,39 @@
-# typed: strict
+# typed: false
 class Dao::Release < ApplicationRecord
   has_many :items, class_name: 'Dao::ReleaseItem', foreign_key: :dao_release_id
+
+  def write(release)
+    self.attributes = {
+      dao_product_id: release.product_id.to_s,
+      title: release.title,
+      can_remove: release.can_remove?
+    }
+
+    items.destroy(*items)
+    release.items.to_a.each do |item|
+      self.items.build(dao_issue_id: item)
+    end
+  end
+
+  def read
+    Release::Release.from_repository(
+      read_release_id,
+      read_product_id,
+      title,
+      read_items
+    )
+  end
+
+  def read_release_id
+    Release::Id.from_string(id)
+  end
+
+  def read_product_id
+    Product::Id.from_string(dao_product_id)
+  end
+
+  def read_items
+    items.map { |item| Issue::Id.from_string(item.dao_issue_id) }
+      .yield_self { |list| Release::ItemList.new(list) }
+  end
 end
