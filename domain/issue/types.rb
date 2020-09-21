1b480e75426b3f33f402333893392f5a89084118
haazime <h4zime@gmail.com>
2020-09-21 14:15:21 +0900

    add task issue type

--- domain/issue/types.rb
+++ domain/issue/types.rb
@@ -2,19 +2,21 @@
 require 'sorbet-runtime'
 require 'issue/type'
 require 'issue/types/feature'
+require 'issue/types/task'
 
 module Issue
   module Types
     class << self
       extend T::Sig
 
-      TYPES = {
+      TYPES = T.let({
         'feature' => Feature,
-      }
+        'task' => Task,
+      }, T::Hash[String, Type])
 
       sig {params(str: String).returns(Type)}
       def from_string(str)
-        TYPES[str]
+        T.must(TYPES[str])
       end
     end
   end
