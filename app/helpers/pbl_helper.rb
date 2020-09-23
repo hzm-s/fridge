8596d52b1f775d88d2fc6734ed0882aead747e2c
haazime <h4zime@gmail.com>
2020-09-23 20:18:16 +0900

    present acceptance criteria status

--- app/helpers/pbl_helper.rb
+++ app/helpers/pbl_helper.rb
@@ -1,9 +1,5 @@
 # typed: false
 module PblHelper
-  def pbl_item_css_classes(item)
-    "pbl-item-outer pbl-item-outer--#{item.status} pbl-item-bg--#{item.type}"
-  end
-
   def sortable_pbl_options(product_id, release_id)
     {
       controller: 'sort-pbl',
@@ -11,5 +7,16 @@ module PblHelper
       sort_pbl_url: product_backlog_path(product_id: product_id),
       sort_pbl_name: "pbl-section-#{release_id}"
     }
+  end
+
+  def pbl_item_css_classes(item)
+    "pbl-item-outer pbl-item-outer--#{item.status} pbl-item-bg--#{item.type}"
+  end
+
+  def pbl_item_criteria_css_classes(item)
+    base = 'pbl-item__ac-trigger'
+    return base if item.criteria.any?
+
+    "#{base} #{base}--empty"
   end
 end
