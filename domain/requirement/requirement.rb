def6271a23b2639ca9ee5445fe1ea768732e5e23
haazime <h4zime@gmail.com>
2020-09-25 20:27:09 +0900

    add module

---
+++ domain/requirement/requirement.rb
@@ -0,0 +1,8 @@
+# typed: strict
+require 'sorbet-runtime'
+
+module Requirement
+  class Requirement
+    extend T::Sig
+  end
+end
---
+++ spec/domain/requirement/requirement_spec.rb
@@ -0,0 +1,7 @@
+# typed: false
+require 'domain_helper'
+
+module Requirement
+  describe Requirement do
+  end
+end
