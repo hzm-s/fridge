b1add6c42f08f661811160e54e70c42c5155e1bb
haazime <h4zime@gmail.com>
2020-10-06 18:05:14 +0900

    fix pbl list view

--- app/helpers/pbl_helper.rb
+++ app/helpers/pbl_helper.rb
@@ -1,11 +1,9 @@
 # typed: false
 module PblHelper
-  def sortable_pbl_options(product_id, release_id)
+  def sortable_pbl_options(product_id)
     {
       controller: 'sort-pbl',
-      release_id: release_id,
-      sort_pbl_url: product_backlog_path(product_id: product_id),
-      sort_pbl_name: "pbl-section-#{release_id}"
+      url: product_backlog_path(product_id: product_id),
     }
   end
 
