56e35c8c3cbfe99e1ba0cd7b4b8c1d7571220c80
haazime <h4zime@gmail.com>
2020-10-09 18:30:08 +0900

    show releases and navs

--- app/controllers/release_plans_controller.rb
+++ app/controllers/release_plans_controller.rb
@@ -1,5 +1,16 @@
+# typed: false
 class ReleasePlansController < ApplicationController
+  include ProductHelper
+
+  helper_method :current_product_id
+
   def show
     @releases = Dao::Release.where(dao_product_id: params[:product_id])
+  end
+
+  private
+
+  def current_product_id
+    params[:product_id]
   end
 end
