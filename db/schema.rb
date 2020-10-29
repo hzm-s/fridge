e498299b0903145d5b3662e03de383b21b376517
haazime <h4zime@gmail.com>
2020-10-29 18:40:48 +0900

    remove Release module

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_10_050340) do
+ActiveRecord::Schema.define(version: 2020_07_12_015347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -78,14 +78,6 @@ ActiveRecord::Schema.define(version: 2020_08_10_050340) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "dao_releases", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.uuid "dao_product_id"
-    t.string "title", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["dao_product_id"], name: "index_dao_releases_on_dao_product_id"
-  end
-
   create_table "dao_team_members", force: :cascade do |t|
     t.uuid "dao_team_id"
     t.uuid "dao_person_id"
@@ -110,7 +102,6 @@ ActiveRecord::Schema.define(version: 2020_08_10_050340) do
   add_foreign_key "dao_issues", "dao_products"
   add_foreign_key "dao_orders", "dao_products"
   add_foreign_key "dao_products", "dao_people", column: "owner_id"
-  add_foreign_key "dao_releases", "dao_products"
   add_foreign_key "dao_team_members", "dao_teams"
   add_foreign_key "dao_teams", "dao_products"
 end
--- spec/support/issue_support.rb
+++ spec/support/issue_support.rb
@@ -4,14 +4,12 @@ require_relative '../domain_support/issue_domain_support'
 module IssueSupport
   include IssueDomainSupport
 
-  def add_issue(product_id, description = 'DESC', type: :feature, acceptance_criteria: [], size: nil, release: nil)
+  def add_issue(product_id, description = 'DESC', type: :feature, acceptance_criteria: [], size: nil)
     issue = perform_add_issue(product_id, Issue::Types.from_string(type.to_s), description)
 
     add_acceptance_criteria(issue, acceptance_criteria) if acceptance_criteria
 
     estimate_feature(issue.id, size) if size
-
-    add_issue_to_release(issue.id, release) if release
 
     IssueRepository::AR.find_by_id(issue.id)
   end
@@ -26,23 +24,12 @@ module IssueSupport
     end
   end
 
-  def add_issue_to_release(issue_id, release_id)
-    ReleaseRepository::AR.find_by_id(release_id)
-      .yield_self { |r| r.tap { |x| x.add_item(issue_id) } }
-      .yield_self { |r| ReleaseRepository::AR.store(r) }
-  end
-
   def estimate_feature(issue_id, size)
     EstimateFeatureUsecase.perform(issue_id, Issue::StoryPoint.new(size))
   end
 
   def remove_issue(issue_id)
     RemoveIssueUsecase.perform(issue_id)
-  end
-
-  def add_release(product_id, title = 'Release2')
-    AddReleaseUsecase.perform(product_id, title)
-      .yield_self { |id| ReleaseRepository::AR.find_by_id(id) }
   end
 
   private
