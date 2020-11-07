0c787e9aa030e3a06ebd5b5662ca742e934e3c49
haazime <h4zime@gmail.com>
2020-11-07 10:01:51 +0900

    Follow new Plan schema

--- app/queries/product_backlog_query.rb
+++ app/queries/product_backlog_query.rb
@@ -5,7 +5,7 @@ module ProductBacklogQuery
     def call(product_id)
       items = fetch_issues(product_id).map { |i| IssueStruct.new(i) }
       order = fetch_order(product_id)
-      ordered_items = order.entries.map { |e| items.find { |i| i.id == e } }
+      ordered_items = order.map { |e| items.find { |i| i.id == e } }
 
       ProductBacklog.new(items: ordered_items)
     end
@@ -17,7 +17,7 @@ module ProductBacklogQuery
     end
 
     def fetch_order(product_id)
-      Dao::Order.find_by(dao_product_id: product_id)
+      Dao::Plan.find_by(dao_product_id: product_id).order
     end
   end
 
