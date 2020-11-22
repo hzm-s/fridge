7b65ac83e909d72dbff7d87d40e6c40cba987265
haazime <h4zime@gmail.com>
2020-11-22 10:12:19 +0900

    clean up

--- domain/plan.rb
+++ domain/plan.rb
@@ -3,8 +3,6 @@ module Plan
   autoload :Plan, 'plan/plan'
   autoload :IssueContainer, 'plan/issue_container'
   autoload :IssueList, 'plan/issue_list'
-  autoload :Scope, 'plan/scope'
-  autoload :ScopeSet, 'plan/scope_set'
   autoload :Release, 'plan/release'
   autoload :PlanRepository, 'plan/plan_repository'
 end
