7f5f68cf62e9ffdfc2689dc799b965b7ae902141
haazime <h4zime@gmail.com>
2020-11-23 10:25:49 +0900

    Fix to create plan

--- domain/plan/plan.rb
+++ domain/plan/plan.rb
@@ -10,7 +10,7 @@ module Plan
 
       sig {params(product_id: Product::Id).returns(T.attached_class)}
       def create(product_id)
-        new(product_id, [], IssueList.new([]))
+        new(product_id, ReleaseList.new, IssueList.new)
       end
 
       sig {params(product_id: Product::Id, order: Order, scopes: ScopeSet).returns(T.attached_class)}
@@ -22,40 +22,17 @@ module Plan
     sig {returns(Product::Id)}
     attr_reader :product_id
 
-    sig {returns(T::Array[Release])}
+    sig {returns(ReleaseList)}
     attr_reader :scoped
 
     sig {returns(IssueList)}
     attr_reader :not_scoped
 
-    sig {params(product_id: Product::Id, scoped: T::Array[Release], not_scoped: IssueList).void}
+    sig {params(product_id: Product::Id, scoped: ReleaseList, not_scoped: IssueList).void}
     def initialize(product_id, scoped, not_scoped)
       @product_id = product_id
       @scoped = scoped
       @not_scoped = not_scoped
-    end
-
-    sig {params(release: Release).void}
-    def add_release(release)
-      @scoped << release
-    end
-
-    sig {params(name: String).void}
-    def remove_release(name)
-      @scoped.delete_if { |r| r.name == name }
-    end
-
-    sig {params(release: Release).void}
-    def update_scoped(release)
-      return if release.have_same_issue?(@not_scoped)
-
-      index = @scoped.find_index { |r| r.name == release.name }
-      @scoped[index] = release
-    end
-
-    sig {params(list: IssueList).void}
-    def update_not_scoped(list)
-      @not_scoped = list
     end
   end
 end
--- spec/domain/plan/plan_spec.rb
+++ spec/domain/plan/plan_spec.rb
@@ -8,9 +8,10 @@ module Plan
     describe 'Create' do
       it do
         plan = described_class.create(product_id)
+
         expect(plan.product_id).to eq product_id
-        expect(plan.scoped).to be_empty
-        expect(plan.not_scoped).to eq IssueList.new([])
+        expect(plan.scoped).to eq ReleaseList.new
+        expect(plan.not_scoped).to eq IssueList.new
       end
     end
 
@@ -20,65 +21,6 @@ module Plan
     let(:issue_c) { Issue::Id.create }
     let(:issue_d) { Issue::Id.create }
     let(:issue_e) { Issue::Id.create }
-
-    describe 'Add release' do
-      it do
-        issues = issue_list(issue_a, issue_b, issue_c)
-        plan.update_not_scoped(issues)
-
-        release = Release.new('MVP', issue_list)
-        plan.add_release(release)
-
-        aggregate_failures do
-          expect(plan.scoped).to eq [release]
-          expect(plan.not_scoped).to eq issues
-        end
-      end
-    end
-
-    describe 'Update release' do
-      it do
-        before = Release.new('MVP', issue_list)
-        plan.add_release(before)
-
-        after = Release.new('MVP', issue_list(issue_a, issue_b))
-        plan.update_scoped(after)
-
-        aggregate_failures do
-          expect(plan.scoped).to eq [after]
-          expect(plan.not_scoped).to eq issue_list
-        end
-      end
-
-      it do
-        plan.update_not_scoped(issue_list(issue_a, issue_b))
-
-        before = Release.new('MVP', issue_list)
-        plan.add_release(before)
-
-        after = Release.new('MVP', issue_list(issue_c, issue_a))
-        plan.update_scoped(after)
-
-        aggregate_failures do
-          expect(plan.scoped).to eq [before]
-          expect(plan.not_scoped).to eq issue_list(issue_a, issue_b)
-        end
-      end
-    end
-
-    describe 'Remove release' do
-      it do
-        release = Release.new('MVP', issue_list)
-        plan.add_release(release)
-
-        plan.remove_release('MVP')
-
-        aggregate_failures do
-          expect(plan.scoped).to eq []
-          expect(plan.not_scoped).to eq issue_list
-        end
-      end
-    end
   end
 end
 __END__
