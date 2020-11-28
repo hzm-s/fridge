407dfeceb1b727f91dac61d66c90ebbd66f219fc
haazime <h4zime@gmail.com>
2020-11-28 10:23:20 +0900

    Fix PlanRepository

--- spec/repositories/plan_repository_spec.rb
+++ spec/repositories/plan_repository_spec.rb
@@ -13,10 +13,14 @@ RSpec.describe PlanRepository::AR do
       issue_c = add_issue(product.id)
 
       plan = described_class.find_by_product_id(product.id)
-      releases = plan.scoped
-      releases = releases.add(Plan::Release.new('Ph1', issue_list(issue_a.id)))
-      releases = releases.add(Plan::Release.new('Ph2', issue_list(issue_b.id)))
-      plan.update_scoped(releases)
+      scoped = Plan::ReleaseList.new([
+        Plan::Release.new('Ph1', issue_list(issue_a.id)),
+        Plan::Release.new('Ph2', issue_list(issue_b.id))
+      ])
+      not_scoped = issue_list(issue_c.id)
+
+      plan.update_scoped(scoped)
+      plan.update_not_scoped(not_scoped)
 
       expect { described_class.store(plan) }
         .to change { Dao::Plan.count }.by(0)
@@ -25,14 +29,14 @@ RSpec.describe PlanRepository::AR do
       rel = Dao::Plan.find_by(dao_product_id: plan.product_id.to_s)
       expect(rel.not_scoped_issues).to eq [issue_c.id.to_s]
       expect(rel.releases.size).to eq 2
-      expect(rel.releases[0].release_id).to eq 'Ph1'
-      expect(rel.releases[0].tail).to eq issue_a.id.to_s
-      expect(rel.releases[1].release_id).to eq 'Ph2'
-      expect(rel.releases[1].tail).to eq issue_b.id.to_s
+      expect(rel.releases[0].name).to eq 'Ph1'
+      expect(rel.releases[0].issues).to eq [issue_a.id.to_s]
+      expect(rel.releases[1].name).to eq 'Ph2'
+      expect(rel.releases[1].issues).to eq [issue_b.id.to_s]
     end
   end
 
-  xdescribe 'Find' do
+  describe 'Find' do
     let(:product) { create_product }
 
     let(:issue_a) { add_issue(product.id) }
@@ -41,20 +45,20 @@ RSpec.describe PlanRepository::AR do
 
     it do
       plan = Plan::Plan.create(product.id)
-      plan.append_issue(issue_a.id)
-      plan.append_issue(issue_b.id)
-      plan.append_issue(issue_c.id)
-      plan.specify_release('Ph1', issue_a.id)
-      plan.specify_release('Ph2', issue_c.id)
 
       described_class.store(plan)
       stored = described_class.find_by_product_id(product.id)
-
-      expect(stored.order).to eq Plan::Order.new([issue_a.id, issue_b.id, issue_c.id])
-      expect(stored.scopes).to eq Plan::ScopeSet.new([
-        Plan::Scope.new('Ph1', issue_a.id),
-        Plan::Scope.new('Ph2', issue_c.id)
+      scoped = Plan::ReleaseList.new([
+        Plan::Release.new('Ph1', issue_list(issue_a.id)),
+        Plan::Release.new('Ph2', issue_list(issue_b.id))
       ])
+      not_scoped = issue_list(issue_c.id)
+
+      stored.update_scoped(scoped)
+      stored.update_not_scoped(not_scoped)
+
+      expect(stored.scoped).to eq scoped
+      expect(stored.not_scoped).to eq not_scoped
     end
   end
 end
