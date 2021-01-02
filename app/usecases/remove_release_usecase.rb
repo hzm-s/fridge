a55b7ffcf6d9e1b266e02baf5a707ad6b4bd1600
haazime <h4zime@gmail.com>
2021-01-02 10:51:06 +0900

    Remove empty release

---
+++ app/usecases/remove_release_usecase.rb
@@ -0,0 +1,21 @@
+# typed: strict
+require 'sorbet-runtime'
+
+class RemoveReleaseUsecase < UsecaseBase
+  extend T::Sig
+
+  sig {void}
+  def initialize
+    @repository = T.let(PlanRepository::AR, Plan::PlanRepository)
+  end
+
+  sig {params(product_id: Product::Id, name: String).void}
+  def perform(product_id, name)
+    plan = @repository.find_by_product_id(product_id)
+
+    new_scheduled = plan.scheduled.remove(name)
+    plan.update_scheduled(new_scheduled)
+
+    @repository.store(plan)
+  end
+end
---
+++ spec/usecases/remove_release_usecase.rb
@@ -0,0 +1,36 @@
+# typed: false
+require 'rails_helper'
+
+RSpec.describe RemoveReleaseUsecase do
+  let(:product) { create_product }
+  let(:issue_a) { add_issue(product.id).id }
+  let(:issue_b) { add_issue(product.id).id }
+
+  before do
+    plan = PlanRepository::AR.find_by_product_id(product.id)
+    plan.update_scheduled(
+      release_list({
+        'R1' => issue_list(issue_a),
+        'R2' => issue_list,
+      })
+    )
+    plan.update_pending(issue_list(issue_b))
+    PlanRepository::AR.store(plan)
+  end
+
+  it do
+    described_class.perform(product.id, 'R2')
+
+    plan = PlanRepository::AR.find_by_product_id(product.id)
+
+    aggregate_failures do
+      expect(plan.scheduled).to eq release_list({ 'R1' => issue_list(issue_a) })
+      expect(plan.pending).to eq issue_list(issue_b)
+    end
+  end
+
+  it do
+    expect { described_class.perform(product.id, 'R1') }
+      .to raise_error(Plan::ReleaseIsNotEmpty)
+  end
+end
