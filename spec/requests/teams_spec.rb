8216db415ba01fd641ef5c83a3ad527061c97258
haazime <h4zime@gmail.com>
2021-01-03 15:28:02 +0900

    show PO as team member

--- spec/requests/teams_spec.rb
+++ spec/requests/teams_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe 'teams' do
 
   let!(:product) { create_product(person: user_account.person_id) }
 
-  describe '#create' do
+  describe 'create' do
     it do
       post teams_path, params: { form: { product_id: product.id.to_s, name: 'KAIHATSU GUMI' } }
       follow_redirect!
@@ -21,6 +21,14 @@ RSpec.describe 'teams' do
     it do
       post teams_path, params: { form: { product_id: product.id, name: '' } }
       expect(response.body).to include I18n.t('errors.messages.blank')
+    end
+  end
+
+  describe 'show' do
+    it do
+      get team_path(resolve_team(product.id).id.to_s)
+
+      expect(response.body).to include user_account.person.name.to_s
     end
   end
 end
