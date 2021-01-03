aaee80a0b20750f5ca73e906213630f61e03f3e5
haazime <h4zime@gmail.com>
2021-01-03 13:19:16 +0900

    Product::Product is not include person

--- domain/product/product.rb
+++ domain/product/product.rb
@@ -8,14 +8,14 @@ module Product
     class << self
       extend T::Sig
 
-      sig {params(owner: Person::Id, name: String, description: T.nilable(String)).returns(T.attached_class)}
-      def create(owner, name, description = nil)
-        new(Id.create, name, owner, description)
+      sig {params(name: String, description: T.nilable(String)).returns(T.attached_class)}
+      def create(name, description = nil)
+        new(Id.create, name, description)
       end
 
-      sig {params(id: Id, name: String, owner: Person::Id, description: T.nilable(String)).returns(T.attached_class)}
-      def from_repository(id, name, owner, description)
-        new(id, name, owner, description)
+      sig {params(id: Id, name: String, description: T.nilable(String)).returns(T.attached_class)}
+      def from_repository(id, name, description)
+        new(id, name, description)
       end
     end
 
@@ -25,17 +25,13 @@ module Product
     sig {returns(String)}
     attr_reader :name
 
-    sig {returns(Person::Id)}
-    attr_reader :owner
-
     sig {returns(T.nilable(String))}
     attr_reader :description
 
-    sig {params(id: Id, name: String, owner: Person::Id, description: T.nilable(String)).void}
-    def initialize(id, name, owner, description)
+    sig {params(id: Id, name: String, description: T.nilable(String)).void}
+    def initialize(id, name, description)
       @id = id
       @name = name
-      @owner = owner
       @description = description
     end
     private_class_method :new
--- spec/domain/product/product_spec.rb
+++ spec/domain/product/product_spec.rb
@@ -3,17 +3,14 @@ require 'domain_helper'
 
 module Product
   RSpec.describe Product do
-    let(:owner) { Person::Id.create }
-
     describe 'Create product' do
       it do
-        product = described_class.create(owner, 'ABC', 'DESC_ABC')
+        product = described_class.create('ABC', 'DESC_ABC')
 
         aggregate_failures do
           expect(product.id).to_not be_nil
           expect(product.name).to eq 'ABC'
           expect(product.description).to eq 'DESC_ABC'
-          expect(product.owner).to eq owner
         end
       end
     end
