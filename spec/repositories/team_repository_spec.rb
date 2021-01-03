5c2d7692252e1cbb56d85ab2ab47f38dc515233a
haazime <h4zime@gmail.com>
2021-01-03 11:01:06 +0900

    follow team has PO

--- spec/repositories/team_repository_spec.rb
+++ spec/repositories/team_repository_spec.rb
@@ -6,14 +6,15 @@ RSpec.describe TeamRepository::AR, type: :repository do
   let(:person_a) { sign_up }
   let(:person_b) { sign_up }
   let(:person_c) { sign_up }
+  let(:person_d) { sign_up }
 
   describe 'add' do
     it do
       team = Team::Team.create('The Team')
       team.develop(product.id)
-
-      team.add_member(sm_member(person_a.person_id))
-      team.add_member(dev_member(person_b.person_id))
+      team.add_member(po_member(person_a.person_id))
+      team.add_member(sm_member(person_b.person_id))
+      team.add_member(dev_member(person_c.person_id))
 
       described_class.add(team)
 
@@ -22,12 +23,14 @@ RSpec.describe TeamRepository::AR, type: :repository do
       expect(team_rels[0].dao_product_id).to eq product.id.to_s
       expect(team_rels[0].name).to eq 'The Team'
 
-      members_rels = Dao::TeamMember.all
-      expect(members_rels.size).to eq 2
-      expect(members_rels[0].dao_person_id).to eq person_a.person_id.to_s
-      expect(members_rels[0].role).to eq 'scrum_master'
-      expect(members_rels[1].dao_person_id).to eq person_b.person_id.to_s
-      expect(members_rels[1].role).to eq 'developer'
+      member_rels = Dao::TeamMember.all
+      expect(member_rels.size).to eq 3
+      expect(member_rels[0].dao_person_id).to eq person_a.person_id.to_s
+      expect(member_rels[0].role).to eq 'product_owner'
+      expect(member_rels[1].dao_person_id).to eq person_b.person_id.to_s
+      expect(member_rels[1].role).to eq 'scrum_master'
+      expect(member_rels[2].dao_person_id).to eq person_c.person_id.to_s
+      expect(member_rels[2].role).to eq 'developer'
     end
   end
 
@@ -35,12 +38,12 @@ RSpec.describe TeamRepository::AR, type: :repository do
     it do
       team = Team::Team.create('The Team')
       team.develop(product.id)
-
-      team.add_member(sm_member(person_a.person_id))
-      team.add_member(dev_member(person_b.person_id))
+      team.add_member(po_member(person_a.person_id))
+      team.add_member(sm_member(person_b.person_id))
+      team.add_member(dev_member(person_c.person_id))
       described_class.add(team)
 
-      team.add_member(dev_member(person_c.person_id))
+      team.add_member(dev_member(person_d.person_id))
 
       described_class.update(team)
 
@@ -49,14 +52,16 @@ RSpec.describe TeamRepository::AR, type: :repository do
       expect(team_rels[0].dao_product_id).to eq product.id.to_s
       expect(team_rels[0].name).to eq 'The Team'
 
-      members_rels = Dao::TeamMember.all
-      expect(members_rels.size).to eq 3
-      expect(members_rels[0].dao_person_id).to eq person_a.person_id.to_s
-      expect(members_rels[0].role).to eq 'scrum_master'
-      expect(members_rels[1].dao_person_id).to eq person_b.person_id.to_s
-      expect(members_rels[1].role).to eq 'developer'
-      expect(members_rels[2].dao_person_id).to eq person_c.person_id.to_s
-      expect(members_rels[2].role).to eq 'developer'
+      member_rels = Dao::TeamMember.all
+      expect(member_rels.size).to eq 4
+      expect(member_rels[0].dao_person_id).to eq person_a.person_id.to_s
+      expect(member_rels[0].role).to eq 'product_owner'
+      expect(member_rels[1].dao_person_id).to eq person_b.person_id.to_s
+      expect(member_rels[1].role).to eq 'scrum_master'
+      expect(member_rels[2].dao_person_id).to eq person_c.person_id.to_s
+      expect(member_rels[2].role).to eq 'developer'
+      expect(member_rels[3].dao_person_id).to eq person_d.person_id.to_s
+      expect(member_rels[3].role).to eq 'developer'
     end
   end
 end
