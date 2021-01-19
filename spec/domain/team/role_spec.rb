c3deb2a9ffa1590c358300e96ae43e92c23b72ff
haazime <h4zime@gmail.com>
2021-01-19 18:09:06 +0900

    not use

--- spec/domain/team/role_spec.rb
+++ spec/domain/team/role_spec.rb
@@ -14,27 +14,5 @@ module Team
       it { expect(Role::Developer).to_not be_can_change_issue_priority }
       it { expect(Role::ScrumMaster).to be_can_change_issue_priority }
     end
-
-    context 'when Product Owner' do
-      it do
-        role = Role::ProductOwner
-        expect(role.denied_actions).to match_array [:estimate_size]
-        expect(role.to_s).to eq 'product_owner'
-      end
-    end
-
-    context 'when Developer' do
-      it do
-        role = Role::Developer
-        expect(role.denied_actions).to match_array [:sort]
-      end
-    end
-
-    context 'when Scrum Master' do
-      it do
-        role = Role::ScrumMaster
-        expect(role.denied_actions).to match_array [:estimate_size]
-      end
-    end
   end
 end
