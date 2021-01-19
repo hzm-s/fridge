cc14cb3ce332fe046ff108a949c93ebe71221bc6
haazime <h4zime@gmail.com>
2021-01-19 18:07:44 +0900

    will add other methods

--- spec/domain/team/role_set_spec.rb
+++ spec/domain/team/role_set_spec.rb
@@ -3,29 +3,31 @@ require 'domain_helper'
 
 module Team
   RSpec.describe RoleSet do
-    it do
-      expect { described_class.new([Role::Developer]) }
-        .to_not raise_error
-    end
+    describe 'create' do
+      it do
+        expect { described_class.new([Role::Developer]) }
+          .to_not raise_error
+      end
 
-    it do
-      expect { described_class.new([Role::ScrumMaster, Role::Developer]) }
-        .to_not raise_error
-    end
+      it do
+        expect { described_class.new([Role::ScrumMaster, Role::Developer]) }
+          .to_not raise_error
+      end
 
-    it do
-      expect { described_class.new([Role::ScrumMaster, Role::ProductOwner]) }
-        .to_not raise_error
-    end
+      it do
+        expect { described_class.new([Role::ScrumMaster, Role::ProductOwner]) }
+          .to_not raise_error
+      end
 
-    it do
-      expect { described_class.new([Role::ProductOwner, Role::Developer]) }
-        .to raise_error InvalidMultipleRoles
-    end
+      it do
+        expect { described_class.new([Role::ProductOwner, Role::Developer]) }
+          .to raise_error InvalidMultipleRoles
+      end
 
-    it do
-      expect { described_class.new([Role::ProductOwner, Role::ScrumMaster, Role::Developer]) }
-        .to raise_error MemberHasTooManyRoles
+      it do
+        expect { described_class.new([Role::ProductOwner, Role::ScrumMaster, Role::Developer]) }
+          .to raise_error MemberHasTooManyRoles
+      end
     end
   end
 end
