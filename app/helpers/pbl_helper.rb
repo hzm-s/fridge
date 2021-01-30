443b47213aaccae09e0cb42beacec2ac69a4e771
haazime <h4zime@gmail.com>
2021-01-30 10:10:26 +0900

    improve UI for change issue priority availability

--- app/helpers/pbl_helper.rb
+++ app/helpers/pbl_helper.rb
@@ -14,6 +14,13 @@ module PblHelper
     "pbl-item-outer pbl-item-outer--#{item.status} pbl-item-bg--#{item.type}"
   end
 
+  def pbl_item_grip_css_classes(can_update)
+    base = "pbl-item__grip js-sortable-handle"
+    return base if can_update
+
+    "#{base} pbl-item__grip--disabled"
+  end
+
   def pbl_item_criteria_css_classes(item)
     base = 'pbl-item__ac-trigger'
     return base unless item.must_have_acceptance_criteria?
