1df128a626390047fb4b300e8e38dc3f51bd6ff6
haazime <h4zime@gmail.com>
2021-03-07 09:54:57 +0900

    not use

--- domain/plan.rb
+++ domain/plan.rb
@@ -3,12 +3,10 @@ module Plan
   class ReleaseIsNotEmpty < StandardError; end
   class NeedAtLeastOneRelease < StandardError; end
   class DuplicatedIssue < StandardError; end
-  class DuplicatedReleaseName < ArgumentError; end
   class PermissionDenied < StandardError; end
 
   autoload :Plan, 'plan/plan'
   autoload :IssueList, 'plan/issue_list'
   autoload :Release, 'plan/release'
-  autoload :ReleaseList, 'plan/release_list'
   autoload :PlanRepository, 'plan/plan_repository'
 end
