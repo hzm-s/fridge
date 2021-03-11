cb2a1c8f5b573a7804a70b4b157597b29fa63d9f
haazime <h4zime@gmail.com>
2021-03-11 18:27:56 +0900

    store muted release

--- spec/repositories/release_repository_spec.rb
+++ spec/repositories/release_repository_spec.rb
@@ -41,4 +41,28 @@ RSpec.describe ReleaseRepository::AR do
       expect(stored.issues).to eq [issue_a, issue_b, issue_c].map(&:to_s)
     end
   end
+
+  describe 'Update' do
+    it do
+      release = Release::Release.create(product_id, 1)
+      described_class.store(release)
+
+      release.append_issue(issue_b)
+      release.append_issue(issue_c)
+      release.append_issue(issue_a)
+
+      expect { described_class.store(release) }
+        .to change { Dao::Release.count }.by(0)
+
+      stored = Dao::Release.last
+
+      expect(stored.dao_product_id).to eq product_id.to_s
+      expect(stored.number).to eq 1
+      expect(stored.issues).to eq [issue_b, issue_c, issue_a].map(&:to_s)
+    end
+  end
+
+  describe 'Remove'
+
+  describe 'Find'
 end
