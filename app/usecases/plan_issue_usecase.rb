8dd726460f081d4f7c0f423dc87b6f52290f4147
haazime <h4zime@gmail.com>
2021-03-18 18:05:49 +0900

    plan issue usecase

--- app/usecases/plan_issue_usecase.rb
+++ app/usecases/plan_issue_usecase.rb
@@ -1,18 +1,27 @@
 # typed: strict
 require 'sorbet-runtime'
 
-class AppendIssueUsecase < UsecaseBase
+class PlanIssueUsecase < UsecaseBase
   extend T::Sig
 
   sig {void}
   def initialize
-    @repository = T.let(IssueRepository::AR, Issue::IssueRepository)
+    @issue_repository = T.let(IssueRepository::AR, Issue::IssueRepository)
+    @plan_repository = T.let(PlanRepository::AR, Plan::PlanRepository)
   end
 
   sig {params(product_id: Product::Id, type: Issue::Type, description: Issue::Description).returns(Issue::Id)}
   def perform(product_id, type, description)
     issue = Issue::Issue.create(product_id, type, description)
-    @repository.store(issue)
+    @issue_repository.store(issue)
+
+    plan = @plan_repository.find_by_product_id(product_id)
+    plan.recent_release.tap do |r|
+      r.append_issue(issue.id)
+      plan.update_release(r)
+    end
+    @plan_repository.store(plan)
+
     issue.id
   end
 end
--- spec/usecases/plan_issue_usecase_spec.rb
+++ spec/usecases/plan_issue_usecase_spec.rb
@@ -1,14 +1,21 @@
 # typed: false
 require 'rails_helper'
 
-RSpec.describe AppendIssueUsecase do
+RSpec.describe PlanIssueUsecase do
   let!(:product) { create_product }
+
+  before do
+    plan = PlanRepository::AR.find_by_product_id(product.id)
+    plan.append_release
+    PlanRepository::AR.store(plan)
+  end
 
   it do
     description = issue_description('ABC')
 
     issue_id = described_class.perform(product.id, Issue::Types::Feature, description)
     issue = IssueRepository::AR.find_by_id(issue_id)
+    plan = PlanRepository::AR.find_by_product_id(product.id)
 
     aggregate_failures do
       expect(issue.product_id).to eq product.id
@@ -17,6 +24,9 @@ RSpec.describe AppendIssueUsecase do
       expect(issue.description).to eq description
       expect(issue.size).to eq Issue::StoryPoint.unknown
       expect(issue.acceptance_criteria).to be_empty
+
+      expect(plan.release_of(1).issues).to eq issue_list(issue_id)
+      expect(plan.release_of(2).issues).to eq issue_list
     end
   end
 end
