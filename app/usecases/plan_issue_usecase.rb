7aafe7b5e6d5ceafbb6251df74627f6fd4758ee2
haazime <h4zime@gmail.com>
2021-03-20 10:54:25 +0900

    move to domain service

--- app/usecases/plan_issue_usecase.rb
+++ app/usecases/plan_issue_usecase.rb
@@ -13,14 +13,11 @@ class PlanIssueUsecase < UsecaseBase
   sig {params(product_id: Product::Id, type: Issue::Type, description: Issue::Description).returns(Issue::Id)}
   def perform(product_id, type, description)
     issue = Issue::Issue.create(product_id, type, description)
-    @issue_repository.store(issue)
 
-    plan = @plan_repository.find_by_product_id(product_id)
-    plan.recent_release.tap do |r|
-      r.append_issue(issue.id)
-      plan.update_release(r)
+    transaction do
+      Plan::AppendIssue.new(@issue_repository, @plan_repository)
+        .append(product_id, issue)
     end
-    @plan_repository.store(plan)
 
     issue.id
   end
---
+++ domain/plan/append_issue.rb
@@ -0,0 +1,28 @@
+# typed: strict
+require 'sorbet-runtime'
+
+module Plan
+  class AppendIssue
+    extend T::Sig
+
+    sig {params(issue_repository: Issue::IssueRepository, plan_repository: PlanRepository).void}
+    def initialize(issue_repository, plan_repository)
+      @issue_repository = issue_repository
+      @plan_repository = plan_repository
+    end
+
+    sig {params(product_id: Product::Id, issue: Issue::Issue).returns(Issue::Issue)}
+    def append(product_id, issue)
+      plan = @plan_repository.find_by_product_id(product_id)
+      plan.recent_release.tap do |r|
+        r.append_issue(issue.id)
+        plan.update_release(r)
+      end
+
+      @issue_repository.store(issue)
+      @plan_repository.store(plan)
+
+      issue
+    end
+  end
+end
