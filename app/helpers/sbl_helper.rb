68ae95f486e5138d5f2f5f0c28dbbdfbcf23f8a4
haazime <h4zime@gmail.com>
2021-05-01 10:07:07 +0900

    sprint backlog item task UI

---
+++ app/helpers/sbl_helper.rb
@@ -0,0 +1,23 @@
+# typed: true
+module SblHelper
+  TASK_PROGRESS_CLASSES = {
+    todo: 'fa fa-minus-circle',
+    wip: 'far fa-circle',
+    done: 'far fa-check-circle'
+  }
+
+  def sbl_item_grip_css_classes(can_move)
+    base = "sbl-item__grip"
+    return base if can_move
+
+    "#{base} sbl-item__grip--disabled"
+  end
+
+  def sbl_task_progress_classes(progress)
+    classes = %w(sbi-task__progress)
+    classes << TASK_PROGRESS_CLASSES[progress]
+    classes << "sbi-task__progress--#{progress}"
+
+    classes.join(' ')
+  end
+end
