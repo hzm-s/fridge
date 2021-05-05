6bb23865ce360ab216e9c995ba0c642f5c66390f
haazime <h4zime@gmail.com>
2021-05-05 09:56:48 +0900

    add plan test helper

--- spec/domain_support/plan_domain_support.rb
+++ spec/domain_support/plan_domain_support.rb
@@ -1,7 +1,28 @@
 # typed: false
 module PlanDomainSupport
+  def create_plan(product_id, issues_per_releases)
+    roles = team_roles(:po)
+
+    plan = Plan::Plan.create(product_id)
+
+    issues_per_releases.each.with_index(1) do |issues, n|
+      release = fetch_or_append_release(plan, n)
+      issues.each { |i| release.plan_issue(i) }
+      plan.update_release(roles, release)
+    end
+
+    plan
+  end
+
   def issue_list(*issue_ids)
     Plan::IssueList.new(issue_ids)
+  end
+
+  def fetch_or_append_release(plan, release_number)
+    plan.release_of(release_number)
+  rescue Plan::ReleaseNotFound
+    plan.append_release(team_roles(:po))
+    retry
   end
 end
 
