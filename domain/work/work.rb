37eb5410fe5b189a85ea6e1d245f44107d1ebf80
haazime <h4zime@gmail.com>
2021-05-16 09:17:33 +0900

    use Array

--- domain/work/work.rb
+++ domain/work/work.rb
@@ -10,20 +10,29 @@ module Work
 
       sig {params(issue_id: Issue::Id).returns(T.attached_class)}
       def create(issue_id)
-        new(issue_id, TaskList.new)
+        new(issue_id, [])
       end
     end
 
     sig {returns(Issue::Id)}
     attr_reader :issue_id
 
-    sig {returns(TaskList)}
+    sig {returns(T::Array[Task])}
     attr_reader :tasks
 
-    sig {params(issue_id: Issue::Id, tasks: TaskList).void}
+    sig {params(issue_id: Issue::Id, tasks: T::Array[Task]).void}
     def initialize(issue_id, tasks)
       @issue_id = issue_id
       @tasks = tasks
+    end
+
+    sig {params(description: String).void}
+    def append_task(description)
+    end
+
+    sig {params(number: Integer).returns(Task)}
+    def task_of(number)
+      @tasks.find { |t| t.number == number }
     end
   end
 end
