857dca63d1d826913ad06e7c85ea7cf852215504
haazime <h4zime@gmail.com>
2021-05-18 18:33:01 +0900

    add creation method for plan task

--- spec/support/sprint_support.rb
+++ spec/support/sprint_support.rb
@@ -4,6 +4,12 @@ module SprintSupport
     StartSprintUsecase.perform(product_id)
       .then { |id| SprintRepository::AR.find_by_id(id) }
   end
+
+  def plan_task(issue_id, task_contents)
+    task_contents.each do |content|
+      PlanTaskUsecase.perform(issue_id, content)
+    end
+  end
 end
 
 RSpec.configure do |c|
