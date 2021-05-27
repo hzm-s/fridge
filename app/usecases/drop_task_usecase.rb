6f441b5b84fbf25946651411739d365b4bf86c07
haazime <h4zime@gmail.com>
2021-05-27 17:23:05 +0900

    add usecase to drop task

---
+++ app/usecases/drop_task_usecase.rb
@@ -0,0 +1,18 @@
+# typed: strict
+require 'sorbet-runtime'
+
+class DropTaskUsecase < UsecaseBase
+  extend T::Sig
+
+  sig {void}
+  def initialize
+    @repository = T.let(WorkRepository::AR, Work::WorkRepository)
+  end
+
+  sig {params(issue_id: Issue::Id, task_number: Integer).void}
+  def perform(issue_id, task_number)
+    work = @repository.find_by_issue_id(issue_id)
+    work.remove_task(task_number)
+    @repository.store(work)
+  end
+end
---
+++ spec/usecases/drop_task_usecase_spec.rb
@@ -0,0 +1,21 @@
+# typed: false
+require 'rails_helper'
+
+RSpec.describe DropTaskUsecase do
+  let(:product) { create_product }
+  let!(:issue) { plan_issue(product.id) }
+
+  it do
+    plan_task(issue.id, %w(Task1 Task2 Task3))
+
+    described_class.perform(issue.id, 2)
+
+    work = WorkRepository::AR.find_by_issue_id(issue.id)
+
+    aggregate_failures do
+      expect(work.task_of(1).content).to eq 'Task1'
+      expect(work.task_of(2)).to be_nil
+      expect(work.task_of(3).content).to eq 'Task3'
+    end
+  end
+end
