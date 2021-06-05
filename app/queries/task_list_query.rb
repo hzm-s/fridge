4e81be39df9e45786e6c6d5cea9f8e8838fb7540
haazime <h4zime@gmail.com>
2021-06-05 10:47:59 +0900

    fix

--- app/queries/task_list_query.rb
+++ app/queries/task_list_query.rb
@@ -2,8 +2,8 @@
 module TaskListQuery
   class << self
     def call(issue_id)
-      work = Dao::Work.find_by(dao_issue_id: issue_id)
-      work.tasks.map { |t| TaskStruct.new(work, t) }
+      work = Dao::Work.eager_load(:tasks).find_by(dao_issue_id: issue_id)
+      work.tasks.map { |t| TaskStruct.new(work.dao_issue_id, t) }
     end
   end
 end
