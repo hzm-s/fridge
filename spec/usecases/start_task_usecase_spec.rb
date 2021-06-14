2df8cc9c8b09d0e662e26419cc93b8b35070dce6
haazime <h4zime@gmail.com>
2021-06-14 18:07:27 +0900

    usecase to start task

---
+++ spec/usecases/start_task_usecase_spec.rb
@@ -0,0 +1,17 @@
+# typed: false
+require 'rails_helper'
+
+RSpec.describe StartTaskUsecase do
+  let(:product) { create_product }
+  let!(:issue) { plan_issue(product.id) }
+
+  it do
+    plan_task(issue.id, %w(Task))
+
+    described_class.perform(issue.id, 1)
+
+    work = WorkRepository::AR.find_by_issue_id(issue.id)
+
+    expect(work.task_of(1).status.to_s).to eq 'wip'
+  end
+end
