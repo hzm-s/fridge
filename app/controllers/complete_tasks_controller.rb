925d354f17f78de153fd0e38dea39644ff0f0ca0
haazime <h4zime@gmail.com>
2021-06-22 17:59:42 +0900

    refactor to use shared template for render

--- app/controllers/complete_tasks_controller.rb
+++ app/controllers/complete_tasks_controller.rb
@@ -3,5 +3,7 @@ class CompleteTasksController < ApplicationController
     @issue_id = Issue::Id.from_string(params[:issue_id])
     CompleteTaskUsecase.perform(@issue_id, params[:number].to_i)
     @tasks = TaskListQuery.call(@issue_id.to_s)
+
+    render :update_task
   end
 end
--- app/controllers/start_tasks_controller.rb
+++ app/controllers/start_tasks_controller.rb
@@ -3,5 +3,7 @@ class StartTasksController < ApplicationController
     @issue_id = Issue::Id.from_string(params[:issue_id])
     StartTaskUsecase.perform(@issue_id, params[:number].to_i)
     @tasks = TaskListQuery.call(@issue_id.to_s)
+
+    render :update_task
   end
 end
