c6f548a40d2c13914c6951f696fa4bbe4aed34c3
haazime <h4zime@gmail.com>
2021-06-23 18:20:18 +0900

    resume task usecase

---
+++ app/usecases/resume_task_usecase.rb
@@ -0,0 +1,18 @@
+# typed: strict
+require 'sorbet-runtime'
+
+class ResumeTaskUsecase < UsecaseBase
+  extend T::Sig
+
+  sig {void}
+  def initialize
+    @repository = T.let(WorkRepository::AR, Work::WorkRepository)
+  end
+
+  sig {params(issue_id: Issue::Id, task_number: Integer).void}
+  def perform(issue_id, task_number)
+    work = T.must(@repository.find_by_issue_id(issue_id))
+    work.resume_task(task_number)
+    @repository.store(work)
+  end
+end
--- spec/support/work_support.rb
+++ spec/support/work_support.rb
@@ -4,6 +4,10 @@ module WorkSupport
   def start_task(issue_id, task_number)
     StartTaskUsecase.perform(issue_id, task_number)
   end
+
+  def suspend_task(issue_id, task_number)
+    SuspendTaskUsecase.perform(issue_id, task_number)
+  end
 end
 
 RSpec.configure do |c|
