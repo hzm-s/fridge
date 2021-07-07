d5120557d5eaed865e26db50d0ec06c7bc5809ae
haazime <h4zime@gmail.com>
2021-07-07 18:29:05 +0900

    add number to dao_acceptance_criteria

--- db/schema.rb
+++ db/schema.rb
@@ -2,8 +2,8 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
@@ -37,9 +37,11 @@ ActiveRecord::Schema.define(version: 2021_05_16_002510) do
 
   create_table "dao_acceptance_criteria", force: :cascade do |t|
     t.uuid "dao_issue_id"
+    t.integer "number", null: false
     t.string "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["dao_issue_id", "number"], name: "index_dao_acceptance_criteria_on_dao_issue_id_and_number", unique: true
     t.index ["dao_issue_id"], name: "index_dao_acceptance_criteria_on_dao_issue_id"
   end
 
@@ -52,7 +54,7 @@ ActiveRecord::Schema.define(version: 2021_05_16_002510) do
     t.index ["dao_sprint_id"], name: "index_dao_assigned_issues_on_dao_sprint_id"
   end
 
-  create_table "dao_issues", id: :uuid, default: -> { "public.gen_random_uuid()" }, force: :cascade do |t|
+  create_table "dao_issues", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.uuid "dao_product_id"
     t.string "issue_type", null: false
     t.string "status", null: false
@@ -71,7 +73,7 @@ ActiveRecord::Schema.define(version: 2021_05_16_002510) do
     t.index ["email"], name: "index_dao_people_on_email", unique: true
   end
 
-  create_table "dao_products", id: :uuid, default: -> { "public.gen_random_uuid()" }, force: :cascade do |t|
+  create_table "dao_products", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name", null: false
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
@@ -119,7 +121,7 @@ ActiveRecord::Schema.define(version: 2021_05_16_002510) do
     t.index ["dao_team_id"], name: "index_dao_team_members_on_dao_team_id"
   end
 
-  create_table "dao_teams", id: :uuid, default: -> { "public.gen_random_uuid()" }, force: :cascade do |t|
+  create_table "dao_teams", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.uuid "dao_product_id"
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
