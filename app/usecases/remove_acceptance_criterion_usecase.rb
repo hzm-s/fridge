5149ff79a191e5f755d0aa19c59bbed400082e2f
haazime <h4zime@gmail.com>
2021-07-10 10:58:36 +0900

    fix remove acceptance criterion usecase

--- app/usecases/remove_acceptance_criterion_usecase.rb
+++ app/usecases/remove_acceptance_criterion_usecase.rb
@@ -9,11 +9,12 @@ class RemoveAcceptanceCriterionUsecase < UsecaseBase
     @repository = T.let(IssueRepository::AR, Issue::IssueRepository)
   end
 
-  sig {params(issue_id: Issue::Id, criterion: Issue::AcceptanceCriterion).void}
-  def perform(issue_id, criterion)
+  sig {params(issue_id: Issue::Id, number: Integer).void}
+  def perform(issue_id, number)
     issue = @repository.find_by_id(issue_id)
 
-    criteria = issue.acceptance_criteria.remove(criterion)
+    criteria = issue.acceptance_criteria
+    criteria.remove(number)
     issue.update_acceptance_criteria(criteria)
 
     @repository.store(issue)
--- spec/usecases/remove_acceptance_criterion_usecase_spec.rb
+++ spec/usecases/remove_acceptance_criterion_usecase_spec.rb
@@ -4,12 +4,15 @@ require 'rails_helper'
 RSpec.describe RemoveAcceptanceCriterionUsecase do
   let!(:product) { create_product }
   let!(:issue) { plan_issue(product.id, acceptance_criteria: %w(AC1 AC2 AC3)) }
-  let(:repository) { IssueRepository::AR }
 
   it do
-    described_class.perform(issue.id, acceptance_criterion('AC2'))
-    stored = repository.find_by_id(issue.id)
+    described_class.perform(issue.id, 2)
+    stored = IssueRepository::AR.find_by_id(issue.id)
 
-    expect(stored.acceptance_criteria).to eq acceptance_criteria(%w(AC1 AC3))
+    aggregate_failures do
+      expect(stored.acceptance_criteria.of(1).content).to eq 'AC1'
+      expect(stored.acceptance_criteria.of(2)).to be_nil
+      expect(stored.acceptance_criteria.of(3).content).to eq 'AC3'
+    end
   end
 end
