65c4b94cc9def81225649068f9a6a6dddd86144e
haazime <h4zime@gmail.com>
2021-07-15 18:11:16 +0900

    modify acceptance criterion content usecase

---
+++ app/usecases/modify_acceptance_criterion_usecase.rb
@@ -0,0 +1,25 @@
+# typed: strict
+require 'sorbet-runtime'
+
+class ModifyAcceptanceCriterionUsecase < UsecaseBase
+  extend T::Sig
+
+  sig {void}
+  def initialize
+    @repository = T.let(IssueRepository::AR, Issue::IssueRepository)
+  end
+
+  sig {params(issue_id: Issue::Id, number: Integer, content: String).void}
+  def perform(issue_id, number, content)
+    issue = @repository.find_by_id(issue_id)
+
+    issue.acceptance_criteria.tap do |criteria|
+      criterion = criteria.of(number)
+      criterion.modify_content(content)
+      criteria.update(criterion)
+      issue.update_acceptance_criteria(criteria)
+    end
+
+    @repository.store(issue)
+  end
+end
---
+++ spec/usecases/modify_acceptance_criterion_usecase_spec.rb
@@ -0,0 +1,18 @@
+# typed: false
+require 'rails_helper'
+
+RSpec.describe ModifyAcceptanceCriterionUsecase do
+  let!(:product) { create_product }
+  let!(:issue) { plan_issue(product.id, acceptance_criteria: %w(AC1 AC2 AC3)) }
+
+  it do
+    described_class.perform(issue.id, 2, 'Modified AC2')
+    stored = IssueRepository::AR.find_by_id(issue.id)
+
+    aggregate_failures do
+      expect(stored.acceptance_criteria.of(1).content).to eq 'AC1'
+      expect(stored.acceptance_criteria.of(2).content).to eq 'Modified AC2'
+      expect(stored.acceptance_criteria.of(3).content).to eq 'AC3'
+    end
+  end
+end
