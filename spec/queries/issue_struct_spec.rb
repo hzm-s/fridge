c7cf1a4122c4ebd72b4e7af42b6d7e3cc7c84ef1
haazime <h4zime@gmail.com>
2021-08-08 10:03:03 +0900

    refactor

--- spec/queries/issue_struct_spec.rb
+++ spec/queries/issue_struct_spec.rb
@@ -6,8 +6,7 @@ RSpec.describe IssueStruct do
 
   it 'returns acceptance criteria' do
     issue = plan_issue(product.id, acceptance_criteria: %w(AC1 AC2 AC3))
-
-    s = described_class.new(Dao::Issue.last)
+    s = build_struct { issue }
 
     aggregate_failures do
       expect(s.criteria.map(&:issue_id).uniq).to eq [issue.id.to_s]
@@ -21,33 +20,32 @@ RSpec.describe IssueStruct do
   end
 
   it 'returns requirement to have acceptance criteria' do
-    issue = plan_issue(product.id, type: Issue::Types::Task)
-
-    s = described_class.new(Dao::Issue.last)
-
+    s = build_struct { plan_issue(product.id, type: :task) }
     expect(s).to_not be_must_have_acceptance_criteria
   end
 
   it 'returns status' do
     issue = plan_issue(product.id)
-
-    s = described_class.new(Dao::Issue.last)
-
+    s = build_struct { issue }
     expect(s.status).to eq issue.status
   end
 
-  it 'returns accept issue activity' do
-    feature =
-      plan_issue(product.id, type: :feature)
-      .then { |i| described_class.new(Dao::Issue.find(i.id)) }
+  it 'returns criteria satisfaction' do
+  end
 
-    task =
-      plan_issue(product.id, type: :task)
-      .then { |i| described_class.new(Dao::Issue.find(i.id)) }
+  it 'returns accept issue activity' do
+    feature = build_struct { plan_issue(product.id, type: :feature) }
+    task = build_struct { plan_issue(product.id, type: :task) }
 
     aggregate_failures do
       expect(feature.accept_issue_activity).to eq :accept_feature
       expect(task.accept_issue_activity).to eq :accept_task
     end
+  end
+
+  private
+
+  def build_struct
+    yield.then { |i| described_class.new(Dao::Issue.find(i.id)) }
   end
 end
