be62afea6491e06782f7944fd661940d18f86c2a
haazime <h4zime@gmail.com>
2021-08-23 18:32:02 +0900

    follow current db schema

--- lib/tasks/export_pbi.rb
+++ lib/tasks/export_pbi.rb
@@ -2,13 +2,11 @@
 
 issues = Dao::Issue.all
 criteria = Dao::AcceptanceCriterion.all
-plans = Dao::Plan.all
 releases = Dao::Release.all
 
 data = {
   issues: issues.map(&:attributes),
   criteria: criteria.map(&:attributes),
-  plans: plans.map(&:attributes),
   releases: releases.map(&:attributes),
 }
 
--- lib/tasks/import_pbi.rb
+++ lib/tasks/import_pbi.rb
@@ -9,14 +9,6 @@ data[:criteria].each do |c|
   p Dao::AcceptanceCriterion.create!(c)
 end
 
-data[:plans].each do |p|
-  p Dao::Plan.create!(p)
-end
-
 data[:releases].each do |r|
   Dao::Release.create!(r)
 end
-
-#SELECT setval('dao_acceptance_criteria_id_seq', (SELECT MAX(id) FROM dao_acceptance_criteria));
-#SELECT setval('dao_plans_id_seq', (SELECT MAX(id) FROM dao_plans));
-#SELECT setval('dao_releases_id_seq', (SELECT MAX(id) FROM dao_releases));
