5de43298a3adb518dd3fb9286daab8f8f558cfa3
hzms <h4zime@gmail.com>
2021-10-17 11:28:44 +0900

    use new postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2021_05_16_002510) do
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
-  create_table "app_user_accounts", id: :uuid, default: -> { "public.gen_random_uuid()" }, force: :cascade do |t|
+  create_table "app_user_accounts", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.uuid "dao_person_id"
     t.string "provider", null: false
     t.string "uid", null: false
@@ -63,7 +63,7 @@ ActiveRecord::Schema.define(version: 2021_05_16_002510) do
     t.index ["dao_product_id"], name: "idx_product_id_on_issues"
   end
 
-  create_table "dao_people", id: :uuid, default: -> { "public.gen_random_uuid()" }, force: :cascade do |t|
+  create_table "dao_people", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
     t.datetime "created_at", precision: 6, null: false
