cd3124772d8f5ab73496ac5c089cd58175f9565a
hzms <h4zime@gmail.com>
2021-10-26 18:08:23 +0900

    add Pbi::Statuses

---
+++ domain/pbi/statuses.rb
@@ -0,0 +1,25 @@
+# typed: strict
+require 'sorbet-runtime'
+
+module Pbi
+  class Statuses < T::Enum
+    extend T::Sig
+
+    class << self
+      extend T::Sig
+
+      sig {params(str: String).returns(T.attached_class)}
+      def from_string(str)
+        deserialize(str)
+      end
+    end
+
+    enums do
+      Preparation = new('preparation')
+      Ready = new('ready')
+      Wip = new('wip')
+      Accepted = new('accepted')
+      Done = new('done')
+    end
+  end
+end
