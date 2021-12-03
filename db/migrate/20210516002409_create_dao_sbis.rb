8605c5356cc3d1e52b47f1b1127b38a9f001554e
hzms <h4zime@gmail.com>
2021-12-03 18:21:17 +0900

    fix Dao::Sbi migration

--- db/migrate/20210516002409_create_dao_sbis.rb
+++ db/migrate/20210516002409_create_dao_sbis.rb
@@ -2,7 +2,7 @@
 class CreateDaoSbis < ActiveRecord::Migration[6.0]
   def change
     create_table :dao_sbis, id: :uuid do |t|
-      t.references :dao_pbi, type: :uuid, foreign_key: true
+      t.references :dao_pbi, type: :uuid, foreign_key: true, unique: true
 
       t.timestamps
     end
